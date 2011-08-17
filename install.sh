7a626397aa860694f30adead777db74281d1123a
Olaf Alders <olaf@wundersolutions.com>
2011-08-17 12:33:50 -0400

    Adds PerlTidy

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 echo $SELF_PATH
 
 ln -sf $SELF_PATH/bashrc ~/.bashrc
+ln -sf $SELF_PATH/perltidyrc ~/.perltidyrc
 ln -sf $SELF_PATH/screenrc ~/.screenrc
 ln -sf $SELF_PATH/vim/vimrc ~/.vimrc
 
