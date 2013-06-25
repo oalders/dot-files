4310c25c2c48070bc6d1d0e9b75875d9b3fb5bb0
Olaf Alders <olaf@wundersolutions.com>
2013-06-25 16:17:33 -0500

    Adds new git aliases.

--- git_config.sh
+++ git_config.sh
@@ -13,13 +13,14 @@ git config --global branch.master.merge refs/heads/master
 git config --global branch.autosetuprebase always
 git config --global push.default matching
 
+git config --global alias.b  'branch'
 git config --global alias.ba 'branch -a'
-git config --global alias.delete-untracked-files 'clean -f -d'
-git config --global alias.co 'checkout'
-git config --global alias.s 'status'
 git config --global alias.changes 'diff --name-status -r'
+git config --global alias.co 'checkout'
+git config --global alias.delete-untracked-files 'clean -f -d'
 git config --global alias.diffstat 'diff --stat -r'
 git config --global alias.pt 'push --tags'
+git config --global alias.st 'status'
 
 # takes a commit name as sole arg
 git config --global alias.whatis "show -s --pretty='tformat:%h (%s, %ad)' --date=short"
