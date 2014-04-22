dc756390a3b51564a7df818e3dfb0cb3cf7bbe37
Olaf Alders <olaf@wundersolutions.com>
2014-04-22 17:44:55 -0400

    Changes global git push default from matching to simple.

--- git_config.sh
+++ git_config.sh
@@ -7,13 +7,6 @@ echo "git config"
 git config --global user.name "Olaf Alders"
 git config --global user.email "olaf@wundersolutions.com"
 
-git config --global color.ui "auto"
-git config --global branch.master.merge refs/heads/master
-git config --global branch.autosetuprebase always
-git config --global merge.conflictstyle diff3
-git config --global push.default matching
-git config --global help.autocorrect 50
-
 git config --global alias.b  'branch'
 git config --global alias.ba 'branch -a'
 git config --global alias.cam 'commit --amend'
@@ -26,12 +19,19 @@ git config --global alias.deleted 'log -1 --'
 git config --global alias.diffstat 'diff --stat -r'
 git config --global alias.dm 'diff -w -M master...HEAD'
 git config --global alias.dom 'diff -w -M origin/master...HEAD'
+git config --global alias.flog 'log --stat --abbrev-commit --relative-date --pretty=oneline'
+git config --global alias.plog "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git config --global alias.prom 'pull --rebase origin master'
 git config --global alias.pt 'push --tags'
 git config --global alias.st 'status'
+git config --global branch.autosetuprebase always
+git config --global branch.master.merge refs/heads/master
+git config --global color.ui "auto"
+git config --global help.autocorrect 50
+git config --global merge.conflictstyle diff3
+git config --global push.default simple
 
 # takes a commit name as sole arg
 git config --global alias.whatis "show -s --pretty='tformat:%h (%s, %ad)' --date=short"
 
-git config --global alias.flog 'log --stat --abbrev-commit --relative-date --pretty=oneline'
-git config --global alias.plog "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
