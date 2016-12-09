42bba91b0c3f739a5a6be23c6e6b5e10a95dc158
Olaf Alders <olaf@wundersolutions.com>
2016-12-09 15:36:11 -0600

    fpp installer no longer depends on install.sh script.

--- install-fpp.sh
+++ install-fpp.sh
@@ -2,10 +2,17 @@
 
 # Install fpp (Facebook Path Picker)
 
-cd ~/local
-mkdir -p src
-cd src
+set -eu -o pipefail
+
+bin_dir=~/local/bin
+src_dir=~/local/src
+
+mkdir -p $bin_dir 
+mkdir -p $src_dir 
+
+pushd $src_dir
+
 rm -rf PathPicker && git clone git@github.com:facebook/PathPicker.git
-cd PathPicker/
-rm -rf ~/local/bin/fpp
-ln -s "$(pwd)/fpp" ~/local/bin/fpp
+pushd PathPicker/
+rm -rf $bin_dir/fpp
+ln -s "$(pwd)/fpp" $bin_dir/fpp
