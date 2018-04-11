809265132bb2fff84de1e71dd0e22542294c36b7
Olaf Alders <olaf@wundersolutions.com>
2018-04-11 13:05:07 -0400

    Use cpm in more places

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-cpanm --installdeps --cpanfile cpan/default.cpanfile .
-cpanm --installdeps --cpanfile cpan/development.cpanfile .
-cpanm --installdeps --cpanfile cpan/pause.cpanfile .
-cpanm --installdeps --cpanfile cpan/OALDERS.cpanfile .
+cpm install --cpanfile cpan/default.cpanfile
+cpm install --cpanfile cpan/development.cpanfile
+cpm install --cpanfile cpan/pause.cpanfile
 
 # Fails to install on OS X
 cpanm --notest Data::Printer::Filter::JSON
-
-#cpm install --cpanfile cpan/default.cpanfile
-#cpm install --cpanfile cpan/development.cpanfile
-#cpm install --cpanfile cpan/pause.cpanfile
