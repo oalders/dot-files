f198aab8f24679110698802ed5364827df6ade16
Olaf Alders <olaf@wundersolutions.com>
2018-08-14 19:54:41 +0000

    Bump plenv default install to 5.26.2

--- install-plenv.sh
+++ install-plenv.sh
@@ -2,8 +2,12 @@
 
 # On OSX plenv will have been installed via homebrew
 
-plenv install 5.26.1
-plenv global 5.26.1
+test -e ~/.plenv || git clone https://github.com/tokuhirom/plenv.git ~/.plenv
+source ~/.bash_profile
+test -e ~/.plenv/plugins/perl-build || git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
+
+plenv install 5.26.2
+plenv global 5.26.2
 plenv install-cpanm
 plenv rehash
 
