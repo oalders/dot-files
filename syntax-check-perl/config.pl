3d2fc230211f8a18bbe5e3242a68cf2af2791250
Olaf Alders <olaf@wundersolutions.com>
2018-11-01 14:14:09 -0400

    Update syntax-check-perl config

--- syntax-check-perl/config.pl
+++ syntax-check-perl/config.pl
@@ -4,7 +4,13 @@ use warnings;
 my $filename = $ENV{PERL_SYNTAX_CHECK_FILENAME} || "";
 
 # must return a hash that represents configuration for syntax_check
-my $config = {};
+my $config = {
+    compile => {
+        inc        => { libs => [ 'lib', 't/lib' ], replace_libs => 1, },
+        use_module => ['lazy'],
+    },
+};
+return $config;
 
 __END__
 =head1 CONFIGURATION EXAMPLE
