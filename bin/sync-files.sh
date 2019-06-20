42c3967cc68622517a76a22c92eaf5eb7b9eb230
Olaf Alders <olaf@wundersolutions.com>
2019-06-20 09:47:33 -0400

    Sync tmux plugins when copying to a host that can't get to the outside world

--- bin/sync-files.sh
+++ bin/sync-files.sh
@@ -12,4 +12,5 @@ TARGET=$1
 rsync -avz ~/dot-files $TARGET:~/
 rsync -avz ~/.vim/plugged $TARGET:~/.vim/
 rsync -avz ~/.vim/autoload $TARGET:~/.vim/
+rsync -avz ~/.tmux/plugins $TARGET:~/.tmux/
 ssh $TARGET "touch ~/.local_vimrc"
