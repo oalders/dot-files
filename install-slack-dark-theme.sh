eb53f40e4361d4d9dcafe459f3d722f91deefe5c
Olaf Alders <olaf@wundersolutions.com>
2019-09-10 11:20:41 -0400

    Add script to install a proper dark mode for Slack

---
+++ install-slack-dark-theme.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Install a proper dark theme for Slack
+
+set -eu -o pipefail
+
+SRC_DIR=~/local/src
+
+mkdir -p $SRC_DIR
+pushd $SRC_DIR
+
+REPO_NAME=slack-black-theme
+
+rm -rf $REPO_NAME && git clone --depth 1 git@github.com:caiceA/$REPO_NAME.git
+
+FONT_FOLDER=muli_font
+rm -rf $FONT_FOLDER && mkdir -p $FONT_FOLDER
+ZIP_NAME=muli.zip
+
+pushd $FONT_FOLDER
+curl https://www.fontsquirrel.com/fonts/download/muli > $ZIP_NAME
+
+unzip muli.zip
+cp *.ttf ~/Library/Fonts/
+popd
+
+npm i -g npx asar
+
+echo $(pwd)
+pushd $REPO_NAME
+./slack-dark-mode.sh
+popd
+
+exit 0
