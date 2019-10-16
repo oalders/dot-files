ecec1a0632701bbf439ec567552420779e44e62f
Olaf Alders <olaf@wundersolutions.com>
2019-10-16 19:15:32 -0400

    Add script to add patched font for oh-my-git on MacOS

---
+++ install-oh-my-git-fonts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Partly stolen from inc/oh-my-git/README.md
+
+# Copy the awesome fonts to ~/.fonts
+cd /tmp
+git clone http://github.com/gabrielelana/awesome-terminal-fonts
+cd awesome-terminal-fonts
+git checkout patching-strategy
+open /tmp/awesome-terminal-fonts/patched/SourceCodePro+Powerline+Awesome+Regular.ttf
