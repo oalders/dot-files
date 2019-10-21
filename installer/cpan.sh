1a73ef8dfd7e8ebef2408a42eb80f61c3b196a7d
Olaf Alders <olaf@wundersolutions.com>
2019-10-21 11:37:45 -0400

    Default global module installs to Plenv default dirs

--- installer/cpan.sh
+++ installer/cpan.sh
@@ -4,14 +4,17 @@ set -eu -o pipefail
 
 source ~/dot-files/bash_functions.sh
 
+HAS_PLENV=$(which plenv);
+
 if [ $IS_MM = false ]; then
-    cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
     cpanm --notest App::cpm
-    if [ $(which plenv) ]; then
+    if [ $HAS_PLENV ]; then
         plenv rehash
+    else
+        cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
     fi
     cpm install -g --cpanfile cpan/development.cpanfile
-    if [ $(which plenv) ]; then
+    if [ $HAS_PLENV ]; then
         plenv rehash
     fi
 fi
