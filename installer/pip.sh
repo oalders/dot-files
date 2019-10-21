d8a680c2eabee0fbde7efbe7c561a170a87af3a0
Olaf Alders <olaf@wundersolutions.com>
2019-10-21 19:07:28 -0400

    Maybe install pip in MacOS

--- installer/pip.sh
+++ installer/pip.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+source ~/dot-files/bash_functions.sh
+
 set -eu -o pipefail
 
 alias python='python3'
 
-function pip_install () {
+function pip_install() {
     PIP=$1
     REQUIREMENTS=$2
 
     # make explicit cases for Travis, MacOS and Linux
     if [ $(which $PIP) ]; then
-        if [[ $USER = "travis" ]]; then
+        if [[ $USER == "travis" ]]; then
             $PIP install -r $REQUIREMENTS
         else
             $PIP install --user --upgrade -r $REQUIREMENTS
@@ -21,10 +23,21 @@ function pip_install () {
     fi
 }
 
+pip_install "pip3" "pip/pip3-requirements.txt"
+
 # The prettysql plugin uses /usr/bin/env python, which finds the first python
 # in the path, which will not be python3, so we'll need to double up on
 # installing sqlparse for now.
+if [[ $IS_DARWIN ]]; then
+    pathadd "/Users/$USER/Library/Python/2.7/bin"
+    if [[ ! $(which pip) ]]; then
+        pushd /tmp
+        curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+        python get-pip.py --user
+        popd
+    fi
+fi
+
 pip_install "pip" "pip/pip-requirements.txt"
-pip_install "pip3" "pip/pip3-requirements.txt"
 
 exit 0
