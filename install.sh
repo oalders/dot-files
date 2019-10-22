fec0d4f03b1ba321c1e1c14dbb73061e3320fe33
Olaf Alders <olaf@wundersolutions.com>
2019-10-21 22:53:48 -0400

    Move yarn logic into installer file

--- install.sh
+++ install.sh
@@ -54,22 +54,7 @@ if [ $IS_DARWIN = false ]; then
     ./installer/linux.sh
 fi
 
-if [[ $(command -v yarnx -v) ]]; then
-    echo "yarn already installed"
-else
-    rm -rf $HOME/.yarn
-    curl -o- -L https://yarnpkg.com/install.sh | bash
-fi
-
-if [ $IS_MM = false ]; then
-    yarn global add $NODE_MODULES || true
-else
-    yarn add $NODE_MODULES || true
-fi
-
-if [ $IS_DARWIN = true ]; then
-    yarn global add alfred-fkill || true
-fi
+./installer/yarn.sh
 
 ./installer/cpan.sh
 exit 0
