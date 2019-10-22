373b58a39dc910d24602133a4eed8d721a7be190
Olaf Alders <olaf@wundersolutions.com>
2019-10-22 11:19:19 -0400

    add cpm to path

--- installer/cpan.sh
+++ installer/cpan.sh
@@ -6,7 +6,9 @@ source ~/dot-files/bash_functions.sh
 
 perl --version
 cpanm --version
+cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 cpanm --verbose --notest App::cpm
+pathadd "$HOME/perl5/bin"
 
 cpm install -g --verbose --cpanfile cpan/development.cpanfile
 cpm install -g --verbose --verbose --verbose --verbose --verbose --verbose --verbose --verbose --verbose --cpanfile cpan/cli.cpanfile
