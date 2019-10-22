a2d9a882a44b0349dab2308e4db4d48105fb2e2b
Olaf Alders <olaf@wundersolutions.com>
2019-10-21 22:44:08 -0400

    pushd before symlinking

--- installer/symlinks.sh
+++ installer/symlinks.sh
@@ -2,38 +2,39 @@
 
 set -eu -o pipefail
 
-source ~/dot-files/bash_functions.sh
+pushd ~/dot-files
+source bash_functions.sh
 
 mkdir -p ~/.cpanreporter
 mkdir -p ~/.re.pl
 mkdir -p ~/.vagrant.d
 mkdir -p ~/.npm-packages
 
-DOT_FILES="~/dot-files"
-
-ln -sf $DOT_FILES/ackrc ~/.ackrc
-ln -sf $DOT_FILES/bashrc ~/.bashrc
-ln -sf $DOT_FILES/bash_profile ~/.bash_profile
-ln -sf $DOT_FILES/cpanreporter/config.ini ~/.cpanreporter/config.ini
-cp $DOT_FILES/dataprinter ~/.dataprinter # Data::Printer doesn't like symlinks
+ln -sf ackrc ~/.ackrc
+ln -sf bashrc ~/.bashrc
+ln -sf bash_profile ~/.bash_profile
+ln -sf cpanreporter/config.ini ~/.cpanreporter/config.ini
+cp dataprinter ~/.dataprinter # Data::Printer doesn't like symlinks
 chmod 700 ~/.dataprinter
 
-ln -sf $DOT_FILES/digrc ~/.digrc
-ln -sf $LINK_FLAG $DOT_FILES/dzil ~/.dzil
-ln -sf $DOT_FILES/gitignore_global ~/.gitignore_global
-ln -sf $DOT_FILES/minicpanrc ~/.minicpanrc
+ln -sf digrc ~/.digrc
+ln -sf $LINK_FLAG dzil ~/.dzil
+ln -sf gitignore_global ~/.gitignore_global
+ln -sf minicpanrc ~/.minicpanrc
 if [ $IS_MM = false ]; then
-    ln -sf $DOT_FILES/npmrc ~/.npmrc
+    ln -sf npmrc ~/.npmrc
 fi
-ln -sf $DOT_FILES/perlcriticrc ~/.perlcriticrc
-ln -sf $DOT_FILES/perltidyrc ~/.perltidyrc
-ln -sf $DOT_FILES/profile ~/.profile
-ln -sf $DOT_FILES/proverc ~/.proverc
-ln -sf $DOT_FILES/psqlrc ~/.psqlrc
-ln -sf $DOT_FILES/re.pl/repl.rc ~/.re.pl/repl.rc
-ln -sf $DOT_FILES/screenrc ~/.screenrc
-ln -sf $LINK_FLAG $DOT_FILES/sqitch ~/.sqitch
-ln -sf $DOT_FILES/tigrc ~/.tigrc
-ln -sf $DOT_FILES/Vagrantfile ~/.vagrant.d/Vagrantfile
+ln -sf perlcriticrc ~/.perlcriticrc
+ln -sf perltidyrc ~/.perltidyrc
+ln -sf profile ~/.profile
+ln -sf proverc ~/.proverc
+ln -sf psqlrc ~/.psqlrc
+ln -sf re.pl/repl.rc ~/.re.pl/repl.rc
+ln -sf screenrc ~/.screenrc
+ln -sf $LINK_FLAG sqitch ~/.sqitch
+ln -sf tigrc ~/.tigrc
+ln -sf Vagrantfile ~/.vagrant.d/Vagrantfile
+
+popd
 
 exit 0
