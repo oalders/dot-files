9b45d0b5754fc792e5c526b624cff209f33acbc6
Olaf Alders <olaf@wundersolutions.com>
2019-10-21 22:13:06 -0400

    Stop using $SELF_PATH for symlink creation

--- installer/symlinks.sh
+++ installer/symlinks.sh
@@ -9,32 +9,31 @@ mkdir -p ~/.re.pl
 mkdir -p ~/.vagrant.d
 mkdir -p ~/.npm-packages
 
-SELF_PATH = "${SELF_PATH}/.."
-echo "self path is $SELF_PATH"
+DOT_FILES="~/dot-files"
 
-ln -sf $SELF_PATH/ackrc ~/.ackrc
-ln -sf $SELF_PATH/bashrc ~/.bashrc
-ln -sf $SELF_PATH/bash_profile ~/.bash_profile
-ln -sf $SELF_PATH/cpanreporter/config.ini ~/.cpanreporter/config.ini
-cp $SELF_PATH/dataprinter ~/.dataprinter # Data::Printer doesn't like symlinks
+ln -sf $DOT_FILES/ackrc ~/.ackrc
+ln -sf $DOT_FILES/bashrc ~/.bashrc
+ln -sf $DOT_FILES/bash_profile ~/.bash_profile
+ln -sf $DOT_FILES/cpanreporter/config.ini ~/.cpanreporter/config.ini
+cp $DOT_FILES/dataprinter ~/.dataprinter # Data::Printer doesn't like symlinks
 chmod 700 ~/.dataprinter
 
-ln -sf $SELF_PATH/digrc ~/.digrc
-ln -sf $LINK_FLAG $SELF_PATH/dzil ~/.dzil
-ln -sf $SELF_PATH/gitignore_global ~/.gitignore_global
-ln -sf $SELF_PATH/minicpanrc ~/.minicpanrc
+ln -sf $DOT_FILES/digrc ~/.digrc
+ln -sf $LINK_FLAG $DOT_FILES/dzil ~/.dzil
+ln -sf $DOT_FILES/gitignore_global ~/.gitignore_global
+ln -sf $DOT_FILES/minicpanrc ~/.minicpanrc
 if [ $IS_MM = false ]; then
-    ln -sf $SELF_PATH/npmrc ~/.npmrc
+    ln -sf $DOT_FILES/npmrc ~/.npmrc
 fi
-ln -sf $SELF_PATH/perlcriticrc ~/.perlcriticrc
-ln -sf $SELF_PATH/perltidyrc ~/.perltidyrc
-ln -sf $SELF_PATH/profile ~/.profile
-ln -sf $SELF_PATH/proverc ~/.proverc
-ln -sf $SELF_PATH/psqlrc ~/.psqlrc
-ln -sf $SELF_PATH/re.pl/repl.rc ~/.re.pl/repl.rc
-ln -sf $SELF_PATH/screenrc ~/.screenrc
-ln -sf $LINK_FLAG $SELF_PATH/sqitch ~/.sqitch
-ln -sf $SELF_PATH/tigrc ~/.tigrc
-ln -sf $SELF_PATH/Vagrantfile ~/.vagrant.d/Vagrantfile
+ln -sf $DOT_FILES/perlcriticrc ~/.perlcriticrc
+ln -sf $DOT_FILES/perltidyrc ~/.perltidyrc
+ln -sf $DOT_FILES/profile ~/.profile
+ln -sf $DOT_FILES/proverc ~/.proverc
+ln -sf $DOT_FILES/psqlrc ~/.psqlrc
+ln -sf $DOT_FILES/re.pl/repl.rc ~/.re.pl/repl.rc
+ln -sf $DOT_FILES/screenrc ~/.screenrc
+ln -sf $LINK_FLAG $DOT_FILES/sqitch ~/.sqitch
+ln -sf $DOT_FILES/tigrc ~/.tigrc
+ln -sf $DOT_FILES/Vagrantfile ~/.vagrant.d/Vagrantfile
 
 exit 0
