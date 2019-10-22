95296bf7b2513d727a838865794d313b9fe397ef
Olaf Alders <olaf@wundersolutions.com>
2019-10-22 09:34:11 -0400

    Less logic in CPAN installer

--- installer/cpan.sh
+++ installer/cpan.sh
@@ -7,27 +7,25 @@ source ~/dot-files/bash_functions.sh
 perl --version
 cpanm --version
 
-HAS_PLENV=$(which plenv);
+HAS_PLENV=$(which plenv)
+
+cpanm --notest App::cpm
+if [ $HAS_PLENV ]; then
+    plenv rehash
+else
+    cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+fi
 
 if [ $IS_DARWIN = true ]; then
     # Install Net::SSLeay on MacOS
     export CPPFLAGS="-I/usr/local/opt/openssl/include"
     export LDFLAGS="-L/usr/local/opt/openssl/lib"
 fi
-
-if [ $IS_MM = false ]; then
-    cpanm --notest App::cpm
-    if [ $HAS_PLENV ]; then
-        plenv rehash
-    else
-        cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
-    fi
-    cpm install -g --cpanfile cpan/development.cpanfile
-    if [ $HAS_PLENV ]; then
-        plenv rehash
-    fi
-fi
-
+cpm install -g --cpanfile cpan/development.cpanfile
 cpm install -g --cpanfile cpan/cli.cpanfile
+
+if [ $HAS_PLENV ]; then
+    plenv rehash
+fi
 
 exit 0
