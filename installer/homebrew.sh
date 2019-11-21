1d59c1e14d1a0850d4043f230412e789d61078ff
Olaf Alders <olaf@wundersolutions.com>
2019-11-21 16:37:16 -0500

    brew unlink node@12 in GitHub build

--- installer/homebrew.sh
+++ installer/homebrew.sh
@@ -15,6 +15,7 @@ if [ $IS_DARWIN = true ]; then
 
     if [[ $IS_GITHUB = true ]]; then
         brew unlink node@6 || true
+        brew unlink node@12 || true
         brew bundle install --file=brew/defaults
     else
         brew bundle install --file=brew/defaults
