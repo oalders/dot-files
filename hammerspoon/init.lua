e219f9b4924927c8905db2af23f2b533d4d28e9d
Olaf Alders <olaf@wundersolutions.com>
2020-03-09 10:40:18 -0400

    Add a hammerspoon config basically stolen from @2shortplanks

---
+++ hammerspoon/init.lua
@@ -0,0 +1,48 @@
+mash_app = {"cmd", "ctrl"}
+
+open_app_action = function(name)
+    return function()
+        hs.application.launchOrFocus(name)
+    end
+end
+
+applescript_action = function(script)
+    return function()
+        hs.osascript.applescript(script)
+    end
+end
+
+function chrome_tab_action( url_substring, url_to_visit_if_tab_not_found )
+    return applescript_action([[
+
+      tell application "Google Chrome"
+          activate
+
+          set foundWindow to false
+          set theTabIndex to -1
+
+          repeat with theWindow in every window
+              set theTabIndex to 0
+              repeat with theTab in every tab of theWindow
+                  set theTabIndex to theTabIndex + 1
+                  if theTab's URL contains "]] .. url_substring .. [[" then
+                      set foundWindow to theWindow
+                      set foundTabIndex to theTabIndex
+                  end if
+              end repeat
+          end repeat
+
+          if foundWindow is not false then
+              set foundWindow's active tab index to foundTabIndex
+              set index of foundWindow to 1
+          else
+              open location "]] .. url_to_visit_if_tab_not_found .. [["
+          end if
+
+      end tell
+
+    ]])
+end
+
+hs.hotkey.bind({"cmd", "option"}, 'l', nil, open_app_action('Slack'))
+hs.hotkey.bind({"cmd", "option"}, 'g', nil,  chrome_tab_action('mail.google.com/mail/u/0','https://mail.google.com/mail/u/0/#inbox'))
--- installer/symlinks.sh
+++ installer/symlinks.sh
@@ -24,6 +24,7 @@ ln -sf $PREFIX/digrc ~/.digrc
 ln -sf $LINK_FLAG $PREFIX/dzil ~/.dzil
 ln -sf $PREFIX/gitignore_global ~/.gitignore_global
 ln -sf $PREFIX/golangci.yml ~/.golangci.yml
+ln -sf $PREFIX/hammerspoon ~/.hammerspoon
 ln -sf $PREFIX/minicpanrc ~/.minicpanrc
 if [ $IS_MM = false ]; then
     ln -sf $PREFIX/npmrc ~/.npmrc
