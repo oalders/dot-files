85719541d9b609d75f05baf627c5e9f0b5a1af54
Olaf Alders <olaf@wundersolutions.com>
2020-08-21 12:39:41 +0000

    Install gofumports in vim configuration

--- configure/vim.sh
+++ configure/vim.sh
@@ -29,6 +29,7 @@ if [[ $HAS_GO = true ]]; then
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.30.0
 
     go get -u golang.org/x/tools/gopls@master
+    GO111MODULE=on go get mvdan.cc/gofumpt/gofumports
 else
     echo "Go not found. Not installing shfmt or golangci-lint"
 fi
