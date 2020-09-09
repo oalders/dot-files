64acef31cab303eaf417662fb003bee14350356a
Olaf Alders <olaf@wundersolutions.com>
2020-09-09 16:54:19 +0000

    Use cpm to install cpm

--- installer/cpan.sh
+++ installer/cpan.sh
@@ -10,10 +10,16 @@ source ~/dot-files/bash_functions.sh
 
 perl --version
 
+# Set up some ENV vars so that global installs go to ~/perl5
+if [ "$HAS_PLENV" = false ]; then
+    if [[ "$IS_MM" = true ]]; then
+        remove_path "/vagrant/dev/bin/cpan"
+    fi
+    cpanm --local-lib=~/perl5 local::lib && eval "$(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)"
+fi
+
 if [[ ! $(which cpm) ]]; then
-    curl -fsSL --compressed https://git.io/cpm >/usr/local/bin/cpm
-    chmod +x /usr/local/bin/cpm
-    cpm --version
+    curl -fsSL --compressed https://git.io/cpm | perl - install --global App::cpm
 fi
 
 if [ "$IS_DARWIN" = true ]; then
