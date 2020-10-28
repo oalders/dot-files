a4dc2e5c5e1cc59da9c8be24e41d3f7b8f1461e0
Olaf Alders <olaf@wundersolutions.com>
2020-10-28 11:17:36 -0400

    set -x later in cz installation

--- installer/cz.sh
+++ installer/cz.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eux -o pipefail
+set -eu -o pipefail
 
 if [[ ("${BASH_VERSINFO[0]}" -lt 4 ) ]]; then
     echo "Need bash version >= 4 to install cz"
@@ -9,6 +9,8 @@ fi
 
 # shellcheck source=bash_functions.sh
 source ~/dot-files/bash_functions.sh
+
+set -x
 
 curl -sS https://raw.githubusercontent.com/apathor/cz/master/cz -o /usr/local/bin/cz
 chmod +x /usr/local/bin/cz
