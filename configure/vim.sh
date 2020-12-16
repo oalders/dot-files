c298ea86f4c4feafa05a8d798b67f7a6f8b38b33
Olaf Alders <olaf@wundersolutions.com>
2020-12-16 18:07:43 +0000

    symlink individual files inside of  rather than the entire folder

--- configure/vim.sh
+++ configure/vim.sh
@@ -13,6 +13,12 @@ mkdir -p ~/.vimtmp
 
 ln -sf "$LINK_FLAG" $PREFIX/vim/ftplugin ~/.vim/ftplugin
 
+mkdir -p ~/.vim/after/syntax/perl
+mkdir -p ~/.vim/after/plugin
+ln -sf "$LINK_FLAG" $PREFIX/vim/after/syntax/perl/heredoc-sql.vim ~/.vim/after/syntax/perl/heredoc-sql.vim
+ln -sf "$LINK_FLAG" $PREFIX/vim/after/plugin/abolish.vim ~/.vim/after/plugin/abolish.vim
+
+exit 0
 if [[ $HAS_GO = true ]]; then
     echo "Installing shfmt"
     go get -u mvdan.cc/sh/v3/cmd/shfmt
@@ -40,8 +46,6 @@ rm ~/.vimrc
 
 ln -sf $PREFIX/vim/vimrc ~/.vimrc
 ln -sf $PREFIX/vim/vim-plug-vimrc ~/.vim/vim-plug-vimrc
-
-ln -sf "$LINK_FLAG" $PREFIX/vim/after ~/.vim/after
 
 if [ "$IS_MM" = true ]; then
     ln -sf ~/local-dot-files/maxmind_local_vimrc ~/.local_vimrc
