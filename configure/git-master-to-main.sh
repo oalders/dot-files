df561b6295ca02cb164c23133366fecaeaad65de
Olaf Alders <olaf@wundersolutions.com>
2021-05-18 14:35:42 +0000

    Add a script to convert Git master to main

---
+++ configure/git-master-to-main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Converting master => main
+# via https://www.kiloloco.com/articles/003-From-Master-To-Main/
+
+git checkout master
+git branch -m master main
+git fetch
+git branch --unset-upstream
+git branch -u origin/main
+git push -u origin main
+git symbolic-ref refs/remotes/origin/HEAD refs/remotes/origin/main
+
+exit
+
+# switch main branch in GH UI
+git push origin --delete master
+git branch -D master
