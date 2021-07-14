873cf0b1bda679762843ff0c2a7f902c657b2dca
Olaf Alders <olaf@wundersolutions.com>
2021-07-14 10:32:02 -0400

    Add prove tab completion

--- fzf_functions.sh
+++ fzf_functions.sh
@@ -29,3 +29,16 @@ tm() {
 
     tmux "$SUBCOMMAND" -t "$SESSION"
 }
+
+# prove
+_fzf_complete_prove() {
+  _fzf_complete --reverse --multi --prompt="prove> " -- "$@" < <(
+      fd -e t
+  )
+}
+
+_fzf_complete_prove_post() {
+    awk '{print $1}'
+}
+
+[ -n "$BASH" ] && complete -F _fzf_complete_prove -o default -o bashdefault prove
