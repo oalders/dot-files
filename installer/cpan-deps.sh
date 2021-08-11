c74796fa2720a8c8f42f3e25ebc4559cfe8df26b
Olaf Alders <olaf@wundersolutions.com>
2021-08-11 13:16:00 -0400

    Install latest ExtUtils::MakeMaker

--- installer/cpan-deps.sh
+++ installer/cpan-deps.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# https://stackoverflow.com/questions/67003619/mac-m1-homebrew-perl-carton-netssleay-is-loading-libcrypto-in-an-unsafe-way
+cpm install ExtUtils::MakeMaker
+
 # shellcheck disable=SC2038
 find cpan/* | xargs -n 1 cpm install -g --cpanfile
 
