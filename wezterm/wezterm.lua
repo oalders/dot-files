5779c13c8578a0c8e3289af6217a37d703be59d0
Olaf Alders <olaf@wundersolutions.com>
2021-10-07 15:00:41 -0400

    Set some wezterm tab colours

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,25 @@
 local wezterm = require 'wezterm';
+
 return {
   font_size = 14.0,
   font = wezterm.font("JetBrainsMono Nerd Font"),
   allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
   color_scheme = "OceanicMaterial",
+
+  colors = {
+    tab_bar = {
+      active_tab = {
+        bg_color = "#0328fc",
+        fg_color = "#c0c0c0",
+      },
+      inactive_tab = {
+        bg_color = "#010d54",
+        fg_color = "#808080",
+      },
+      new_tab = {
+        bg_color = "#010d54",
+        fg_color = "#808080",
+      },
+    }
+  }
 }
