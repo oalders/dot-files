1ff48a8b5089064ebc1b57f94818d2f2387364cd
Olaf Alders <olaf@wundersolutions.com>
2021-10-07 09:44:29 -0400

    Use JetBrainsMono Nerd Font in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,7 @@
+local wezterm = require 'wezterm';
 return {
+  font_size = 14.0,
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
   color_scheme = "OceanicMaterial",
 }
