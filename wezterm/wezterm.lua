dfa3f76fe2cb42d1d245c08656f4cf4c0a971a3f
Olaf Alders <olaf@wundersolutions.com>
2021-10-07 17:28:08 -0400

    Use font_with_fallback for wezterm
    The pencil icon wasn't being found in the default font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,10 @@ local wezterm = require 'wezterm';
 
 return {
   font_size = 14.0,
-  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font = wezterm.font_with_fallback({
+    "JetBrainsMono Nerd Font",
+    "DejaVu Sans Mono for Powerline",
+}),
   allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
   color_scheme = "OceanicMaterial",
 
