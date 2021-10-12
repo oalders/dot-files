38485d30173fe921d2435cb8f71745ca2ff75cbf
Olaf Alders <olaf@wundersolutions.com>
2021-10-12 15:15:46 -0400

    Install powerline fonts
    so that we can get "DejaVu Sans Mono for Powerline"

--- installer/fonts.sh
+++ installer/fonts.sh
@@ -9,4 +9,10 @@ fi
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono-nerd-font
 
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
 exit 0
