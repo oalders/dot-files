dac67ff6e9342398c1fba3209f9df43c1a3b64b9
Olaf Alders <olaf@wundersolutions.com>
2021-10-22 09:28:21 -0400

    Remove more brew packages

--- installer/homebrew-maintenance.sh
+++ installer/homebrew-maintenance.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 brew remove bats
+brew remove bison
+brew remove diff-so-fancy
+brew remove fd
+brew remove fpp
+brew remove geoip
+brew remove hub
 brew remove lua@5.1
+brew remove pgcli
+brew remove mercurial
+brew remove mysql
+brew remove postgresql
 rm -rf /usr/local/etc/luarocks
 rm -rf /usr/local/etc/luarocks51
 brew remove truncate
