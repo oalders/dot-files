25c425f72da71b7b0bf678ea4a6dc0f933c8027f
Olaf Alders <olaf@wundersolutions.com>
2021-11-23 20:26:33 -0500

    Remove some steps from git-master-to-main.sh

--- configure/git-master-to-main.sh
+++ configure/git-master-to-main.sh
@@ -3,12 +3,12 @@
 # Converting master => main
 # via https://www.kiloloco.com/articles/003-From-Master-To-Main/
 
-git checkout master
-git branch -m master main
+#git checkout master
+#git branch -m master main
 git fetch
 git branch --unset-upstream
 git branch -u origin/main
-git push -u origin main
+#git push -u origin main
 git symbolic-ref refs/remotes/origin/HEAD refs/remotes/origin/main
 
 exit
