1c67719cb305249eb8a5a2c87e423f7b8c20795e
Olaf Alders <olaf@wundersolutions.com>
2021-11-30 15:46:52 +0000

    Don't install ubi if it's already available

--- installer/ubi.sh
+++ installer/ubi.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+if [ "$(which ubi)" ]; then
+    exit
+fi
+
 curl --silent --location \
     https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
     TARGET=~/local/bin sh
