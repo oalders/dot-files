06edb7f799c7a514133fad065cfabaed1fa1e4a4
Olaf Alders <olaf@wundersolutions.com>
2022-01-03 12:24:05 -0500

    Mention people in Slack without highlighting them

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -49,6 +49,59 @@ hs.hotkey.bind(my_hotkeys, "r", function()
   hs.notify.new({title="Hammerspoon", informativeText="config reloaded"}):send()
 end)
 
+function currentSelection()
+   local elem=hs.uielement.focusedElement()
+   local sel=nil
+   if elem then
+      sel=elem:selectedText()
+   end
+   if (not sel) or (sel == "") then
+      hs.eventtap.keyStroke({"cmd"}, "c")
+      hs.timer.usleep(20000)
+      sel=hs.pasteboard.getContents()
+   end
+   return (sel or "")
+end
+
+-- Transform names via an inexact, but hopefully readable, substitution of
+-- Greek letters. This allows me to use names in Slack without highlighting the
+-- person in question.
+-- Borrowed from https://nikhilism.com/post/2021/useful-hammerspoon-tips/
+function slackifyName()
+    name = currentSelection()
+    greek = {
+        a = "α",
+        b = "β",
+        d = "δ",
+        D = "Δ",
+        e = "ε",
+        f = "φ",
+        F = "Φ",
+        --g = "γ",
+        G = "Γ",
+        i = "ι",
+        k = "κ",
+        l = "λ",
+        m = "μ",
+        n = "ν",
+        O = "Ω",
+        o = "ω",
+        P = "Π",
+        p = "π",
+        ph = "φ",
+        t = "τ",
+        th = "θ",
+        v = "φ",
+    }
+    for key, value in pairs(greek) do
+        name = name:gsub(key, value)
+    end
+
+    hs.pasteboard.setContents(name)
+    hs.timer.usleep(20000)
+    hs.eventtap.keyStroke({"cmd"}, "v")
+end
+
 hs.hotkey.bind(my_hotkeys, 'c', nil, open_app_action('Google Chrome'))
 hs.hotkey.bind(my_hotkeys, 'g', nil, chrome_tab_action('mail.google.com/mail/u/0','https://mail.google.com/mail/u/0/#inbox'))
 hs.hotkey.bind(my_hotkeys, 'i', nil, open_app_action('wezterm'))
@@ -57,3 +110,4 @@ hs.hotkey.bind(my_hotkeys, 'm', nil, chrome_tab_action('https://meet.google.com/
 hs.hotkey.bind(my_hotkeys, 'n', nil, chrome_tab_action('https://github.com/notifications','https://github.com/notifications'))
 hs.hotkey.bind(my_hotkeys, 'o', nil, chrome_tab_action('https://www.irccloud.com/irc/','https://www.irccloud.com/irc/magnet/channel/metacpan'))
 hs.hotkey.bind(my_hotkeys, 'p', nil, chrome_tab_action('https://www.pivotaltracker.com/','https://www.pivotaltracker.com/'))
+hs.hotkey.bind(my_hotkeys, 'q', slackifyName)
