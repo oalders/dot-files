8356f3171d3235b43407d8abab9f9aa71fa52617
Olaf Alders <olaf@wundersolutions.com>
2022-03-14 17:42:09 -0400

    Switch active window to next screen via hyper + n

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -224,3 +224,14 @@ local open_slack_threads = function(name)
     end
 end
 hs.hotkey.bind(hyper, "t", nil, open_slack_threads("Slack"))
+
+-- Stolen from Lukas https://stackoverflow.com/a/58662204/406224
+hs.hotkey.bind(hyper, 'n', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
