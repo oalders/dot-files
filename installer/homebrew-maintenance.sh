b17c2a93e10075accaf4cd5b3725f5455f78d2bb
Olaf Alders <olaf@wundersolutions.com>
2022-03-21 13:47:00 -0400

    Remove more homebrew stuff

--- installer/homebrew-maintenance.sh
+++ installer/homebrew-maintenance.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu -o pipefail
+#set -eu -o pipefail
 
 # shellcheck source=bash_functions.sh
 source ~/dot-files/bash_functions.sh
@@ -10,16 +10,27 @@ set -x
 brew remove bats
 brew remove bison
 brew remove diff-so-fancy
+brew remove erlang@20
 brew remove fd
 brew remove fpp
 brew remove geoip
+brew remove ghc@8.2
 brew remove hub
+brew remove isl@0.18
 brew remove lua@5.1
 brew remove mercurial
 brew remove mysql
+brew remove node@10
 brew remove pgcli
+brew remove php@5.6
+brew remove php56-mcrypt
+brew remove phppgadmin
 brew remove plenv
 brew remove postgresql
+brew remove python@2
+
+brew unlink md5sha1sum
+
 rm -rf /usr/local/etc/luarocks
 rm -rf /usr/local/etc/luarocks51
 brew remove truncate
@@ -28,4 +39,4 @@ if [ "$IS_MM" = false ]; then
     brew remove virtualbox
 fi
 
-brew unlink md5sha1sum
+brew untap caskroom/cask
