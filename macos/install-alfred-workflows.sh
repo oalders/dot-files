b437d15c8c4a0f4a73dc3da0a107edd9fb3aab48
Olaf Alders <olaf@wundersolutions.com>
2022-04-12 10:54:36 -0400

    Use forked alfred-vpn-manager

--- macos/install-alfred-workflows.sh
+++ macos/install-alfred-workflows.sh
@@ -16,8 +16,13 @@ wait_for_input() {
     read -n 1 -s -r -p "Press any key to continue"
 }
 
+
 wait_for_input 'https://github.com/alexchantastic/alfred-ip-address-workflow/releases/download/v1.0.3' 'alfred-ip-address-workflow.alfredworkflow'
-wait_for_input 'https://github.com/deanishe/alfred-vpn-manager/releases/download/v3.2.0' 'VPN-Manager-3.2.alfredworkflow'
+
+# wait_for_input 'https://github.com/deanishe/alfred-vpn-manager/releases/download/v3.2.0' 'VPN-Manager-3.2.alfredworkflow'
+# Temporarily switch to fork of alfred-vpn-manager. See https://github.com/deanishe/alfred-vpn-manager/pull/20
+wait_for_input 'https://github.com/rombarcz/alfred-vpn-manager/releases/download/v3.3.0' 'VPN-Manager-3.3.alfredworkflow'
+
 wait_for_input 'https://github.com/epilande/alfred-browser-tabs/releases/download/v1.0.5' 'Browser-Tabs.alfredworkflow'
 wait_for_input 'https://github.com/gharlan/alfred-github-workflow/releases/download/v1.7.1' 'github.alfredworkflow'
 wait_for_input 'https://github.com/jsumners/alfred-emoji/releases/download/v1.11.1' 'alfred-emoji.alfredworkflow'
