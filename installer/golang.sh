90dd8d9a80bec6489791cf167802de69777623ac
Olaf Alders <olaf@wundersolutions.com>
2022-05-13 12:23:07 -0400

    Fix path to /tmp in golang installer

--- installer/golang.sh
+++ installer/golang.sh
@@ -2,7 +2,7 @@
 
 # https://www.reddit.com/r/golang/comments/tfvb6i/comment/i0ye1r1/?utm_source=share&utm_medium=web2x&context=3
 
-cd tmp || exit 1
+cd /tmp || exit 1
 wget "https://go.dev/dl/$(curl https://go.dev/VERSION?m=text).linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go/
 sudo tar -C /usr/local -xzf "$(curl https://go.dev/VERSION?m=text).linux-amd64.tar.gz"
