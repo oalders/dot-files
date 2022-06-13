3ac52ecd3b8329ef69bc478b70c2cc54d28b792c
Olaf Alders <olaf@wundersolutions.com>
2022-06-13 10:51:57 -0400

    Bump OPENSSL_PREFIX on macOS for installing Perl modules

--- installer/cpan.sh
+++ installer/cpan.sh
@@ -32,7 +32,7 @@ if [[ ! $(which cpm) ]]; then
 fi
 
 if [ "$IS_DARWIN" = true ]; then
-    OPENSSL_PREFIX="/usr/local/Cellar/openssl@1.1/1.1.1n"
+    OPENSSL_PREFIX="/usr/local/Cellar/openssl@1.1/1.1.1o"
     if [[ ! -e $OPENSSL_PREFIX ]]; then
         echo "$OPENSSL_PREFIX does not exist"
         exit 1
