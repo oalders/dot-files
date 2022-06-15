4451d2ba839ffcc6fe03cf6a1d3b806c57abe331
Olaf Alders <olaf@wundersolutions.com>
2022-06-15 16:17:12 +0000

    Install a newer tmux in GitHub workflow

---
+++ installer/tmux.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# shellcheck source=bash_functions.sh
+source ~/dot-files/bash_functions.sh
+
+set -x
+
+if [[ $IS_DARWIN = false ]] && [[ $IS_SUDOER = true ]]; then
+    RELEASE=tmux-3.3a
+    ARCHIVE="$RELEASE.tar.gz"
+    cd /tmp || exit
+    rm -rf $ARCHIVE $RELEASE
+    curl --fail --location "https://github.com/tmux/tmux/releases/download/3.3a/$ARCHIVE" -o $ARCHIVE
+    tar xzvf $ARCHIVE
+    cd $RELEASE
+    ./configure && make
+    sudo make install
+
+    # Also remove any remaining server sockets, or we may be unable to restart tmux.
+    # https://github.com/tmux/tmux/issues/2376#issuecomment-695195592
+    rm -rf /tmp/tmux*
+fi
