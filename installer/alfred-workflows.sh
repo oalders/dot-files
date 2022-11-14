53aa422da50d65f677717f2bdf709b3b6e855778
Olaf Alders <olaf@wundersolutions.com>
2022-11-14 18:07:42 -0500

    Add Div alfred workflow

--- installer/alfred-workflows.sh
+++ installer/alfred-workflows.sh
@@ -16,12 +16,12 @@ wait_for_input() {
     read -n 1 -s -r -p "Press any key to continue"
 }
 
-
 wait_for_input 'https://github.com/alexchantastic/alfred-ip-address-workflow/releases/download/v1.0.3' 'alfred-ip-address-workflow.alfredworkflow'
 wait_for_input 'https://github.com/epilande/alfred-browser-tabs/releases/download/v1.0.5' 'Browser-Tabs.alfredworkflow'
 wait_for_input 'https://github.com/gharlan/alfred-github-workflow/releases/download/v1.7.1' 'github.alfredworkflow'
 wait_for_input 'https://github.com/jsumners/alfred-emoji/releases/download/v1.12.0' 'alfred-emoji.alfredworkflow'
 wait_for_input 'https://github.com/mrodalgaard/alfred-network-workflow/releases/download/v1.1' 'Network.alfredworkflow'
+wait_for_input 'https://github.com/packal/repository/raw/master/com.pawelgrzybek.div' 'div.alfredworkflow'
 wait_for_input 'https://github.com/ruedap/alfred-font-awesome-workflow/releases/download/v5.15.3.1' 'Font-Awesome.alfredworkflow'
 
 REPO=alfred-metacpan-workflow
