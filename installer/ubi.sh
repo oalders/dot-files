a16df9f885d5cca2407475ac574ea7b23495d642
Olaf Alders <olaf@wundersolutions.com>
2022-11-14 17:16:06 -0500

    Change order of ubi installation

--- installer/ubi.sh
+++ installer/ubi.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
-if [ "$(which ubi)" ]; then
-    set -x
-    INSTALL_DIR="$HOME/local/bin"
-    ubi --project houseabsolute/ubi --in $INSTALL_DIR
+INSTALL_DIR="$HOME/local/bin"
 
-    ubi --project houseabsolute/precious --in $INSTALL_DIR
-    ubi --project sharkdp/bat --in $INSTALL_DIR
-    ubi --project Wilfred/difftastic --exe difft --in $INSTALL_DIR
-    # ubi -d --project sharkdp/fd --in $INSTALL_DIR
-    exit
+if [ ! "$(command -v ubi)" ]; then
+    curl --silent --location \
+        https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
+        TARGET=$INSTALL_DIR sh
 fi
 
-curl --silent --location \
-    https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
-    TARGET=~/local/bin sh
+ubi --project houseabsolute/ubi --in "$INSTALL_DIR"
+ubi --project houseabsolute/precious --in "$INSTALL_DIR"
+# ubi --project sharkdp/bat --in "$INSTALL_DIR"
+# ubi --project sharkdp/fd --in $INSTALL_DIR
+# ubi --project Wilfred/difftastic --exe difft --in "$INSTALL_DIR"
+
+exit
