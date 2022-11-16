4f637eea8f0ee63f1d967bb658b7c1db665e4cb2
Olaf Alders <olaf@wundersolutions.com>
2022-11-16 14:36:53 -0500

    Don't install google-java-format by default on macOS

--- installer/maven.sh
+++ installer/maven.sh
@@ -6,7 +6,7 @@ if [ "$IS_DARWIN" = false ]; then
     exit 0
 fi
 
-brew install maven
+brew install google-java-format maven
 brew cask install oracle-jdk
 open vscode:extension/vscjava.vscode-java-pack
 
