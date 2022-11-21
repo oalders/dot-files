63d9d6590f72fa57ff08373335d49f721c8671d3
Olaf Alders <olaf@wundersolutions.com>
2022-11-21 11:41:24 -0500

    Rely on homebrew to link to the latest python

--- installer/pip.sh
+++ installer/pip.sh
@@ -24,17 +24,12 @@ pip_install() {
 pip_install "pip3" "pip/pip3-requirements.txt"
 
 if [ "$IS_DARWIN" = true ]; then
-    # maybe add path if pipx has just been installed
-    remove_path "$HOME/Library/Python/3.9/bin"
-    add_path "$HOME/Library/Python/3.10/bin"
-
-    /usr/local/opt/python@3.10/bin/python3.10 -m pip install --upgrade pip
+    python3 -m pip install --upgrade pip
     pip3 install ansible
-
 fi
 
 # if [ "$(which pipx)" ]; then
-    # pipx install dunk
+# pipx install dunk
 # fi
 
 exit 0
