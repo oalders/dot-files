3a4ecabe24f68f2cd1f2ce7d36e137b2a6eabad6
Olaf Alders <olaf@wundersolutions.com>
2023-01-20 17:54:06 -0500

    Make golang installer work on Macs with Apple silicon

--- installer/golang.sh
+++ installer/golang.sh
@@ -3,6 +3,19 @@
 # https://www.reddit.com/r/golang/comments/tfvb6i/comment/i0ye1r1/?utm_source=share&utm_medium=web2x&context=3
 
 cd /tmp || exit 1
-wget "https://go.dev/dl/$(curl https://go.dev/VERSION?m=text).linux-amd64.tar.gz"
+set -eux
+
+LATEST=$(curl https://go.dev/VERSION?m=text)
+echo "version $LATEST"
+
+ARCHITECTURE="amd"
+
+if [[ $(uname -m) = "aarch64" ]]; then
+    ARCHITECTURE="arm"
+fi
+
+FILENAME="$LATEST".linux-"$ARCHITECTURE"64.tar.gz
+
+curl --location -O "https://go.dev/dl/$FILENAME"
 sudo rm -rf /usr/local/go/
-sudo tar -C /usr/local -xzf "$(curl https://go.dev/VERSION?m=text).linux-amd64.tar.gz"
+sudo tar -C /usr/local -xzf "$FILENAME"
