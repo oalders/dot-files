b82e9765fe83eed97f940fc2c586dbd6f6315249
Olaf Alders <olaf@wundersolutions.com>
2023-03-30 00:10:41 +0000

    Use more vars in oh-my-posh installer

--- installer/oh-my-posh.sh
+++ installer/oh-my-posh.sh
@@ -2,15 +2,17 @@
 
 set -eu
 
+INSTALL_DIR="$HOME/local/bin"
 # shellcheck source=bash_functions.sh
 source ~/dot-files/bash_functions.sh
+add_path "$INSTALL_DIR"
 
 # Pass a version arg if you want to install oh-my-posh on macOS"
 # e.g. ./installer/oh-my-posh.sh v8.27.0"
 
 set -x
 
-CMD="ubi -p JanDeDobbeleer/oh-my-posh --in ~/local/bin"
+CMD="ubi -p JanDeDobbeleer/oh-my-posh --in $INSTALL_DIR"
 
 if [[ ${1+x} ]]; then
     CMD="$CMD --tag $1"
