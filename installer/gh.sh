58bb01ac0f631080cb049780323b601232776b19
Olaf Alders <olaf@wundersolutions.com>
2023-05-21 19:29:56 +0000

    Bump gh version from 2.11.3 to 2.29.0 on linux

--- installer/gh.sh
+++ installer/gh.sh
@@ -6,6 +6,6 @@ FILE=gh.deb
 pushd /tmp || exit 1
 
 rm -rf $FILE
-curl --location https://github.com/cli/cli/releases/download/v2.11.3/gh_2.11.3_linux_amd64.deb -o $FILE
+curl --location https://github.com/cli/cli/releases/download/v2.29.0/gh_2.29.0_linux_amd64.deb -o $FILE
 sudo dpkg -i $FILE
 popd
