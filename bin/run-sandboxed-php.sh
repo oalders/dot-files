ae6e60bd6b37f6e1e82a1cf834895a77551b8f48
Olaf Alders <olaf@wundersolutions.com>
2023-05-23 17:19:40 -0400

    Add composer install to sandboxed PHP setup

--- bin/run-sandboxed-php.sh
+++ bin/run-sandboxed-php.sh
@@ -8,4 +8,4 @@ docker run                                                  \
     -v "$HOME/dot-files/bashrc-docker:/root/.bashrc"        \
     -v "$HOME/dot-files/docker-utils:/root/docker-utils"    \
     arm64v8/php                                             \
-    bash -c "./root/docker-utils/install-composer.sh && cd /sandbox && php -S 0.0.0.0:8000"
+    bash -c "./root/docker-utils/install-composer.sh && cd /sandbox && composer install && php -S 0.0.0.0:8000"
