65c7ca0512502b4519c3b225e577db8a7f9efe03
Olaf Alders <olaf@wundersolutions.com>
2023-05-31 10:26:12 -0400

    Don't try to install go dev binaries if we don't have go

--- configure/vim.sh
+++ configure/vim.sh
@@ -27,7 +27,7 @@ if eval is there go; then
     go install mvdan.cc/sh/v3/cmd/shfmt@latest
     go install mvdan.cc/gofumpt@latest
 else
-    echo "Go not found. Not installing shfmt or golangci-lint"
+    echo "Go not found. Not installing shfmt or gofumpt"
 fi
 
 # vim
@@ -58,7 +58,9 @@ ln -sf "$LINK_FLAG" $PREFIX/vim/after/plugin/abolish.vim ~/.vim/after/plugin/abo
 # This takes forever to run in CI, so we'll just do it here so that we can skip the step on GH
 if [[ $IS_GITHUB == false ]]; then
     add_path ~/local/bin
-    nvim +':GoUpdateBinaries' +qa || true
+    if eval is there go; then
+        nvim +':GoUpdateBinaries' +qa || true
+    fi
     nvim +'PlugInstall --sync' +qa
     nvim +'PlugUpdate --sync' +qa
 fi
