89cc1f0f5d0dc3074720e6f54cb06b6e434c6124
Olaf Alders <olaf@wundersolutions.com>
2023-07-10 11:25:54 -0400

    rehash if we have plenv

--- installer/cpan-deps.sh
+++ installer/cpan-deps.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+set -eux
+
 # https://stackoverflow.com/questions/67003619/mac-m1-homebrew-perl-carton-netssleay-is-loading-libcrypto-in-an-unsafe-way
 cpm install ExtUtils::MakeMaker
 
 # shellcheck disable=SC2038
 find cpan/* | xargs -n 1 cpm install -g --cpanfile
 
-plenv rehash
+if is there plenv;
+  then plenv rehash
+fi
