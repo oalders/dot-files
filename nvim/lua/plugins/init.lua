c1c737188439a75f86cbc85ff13d48645528062a
Olaf Alders <olaf@wundersolutions.com>
2023-07-10 11:35:40 -0400

    Use lua_ls setup from server_configurations.md

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -207,7 +207,28 @@ require("lsp-format").setup {}
 
 require'lspconfig'.bashls.setup {}
 require'lspconfig'.docker_compose_language_service.setup{}
-require'lspconfig'.lua_ls.setup {}
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 require'lspconfig'.yamlls.setup{}
 
 local navbuddy = require("nvim-navbuddy")
