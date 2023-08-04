2f3f28c79843fb5e722692b5ba3b172839f3829e
Olaf Alders <olaf@wundersolutions.com>
2023-08-04 19:47:24 +0000

    Tidy lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,7 +8,7 @@ require 'nvim-treesitter.configs'.setup {
     -- ensure_installed = 'all',
     highlight = {
         enable = true, -- false will disable the whole extension
-        disable = {}, -- list of language that will be disabled
+        disable = {},  -- list of language that will be disabled
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
