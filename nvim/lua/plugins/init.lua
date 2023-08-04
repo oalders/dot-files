8630c0d1c7bf7543dfa18d8e3ff9b8e81ab408df
Olaf Alders <olaf@wundersolutions.com>
2023-08-04 19:46:57 +0000

    Load hlchunk with a custom keybinding

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -364,19 +364,29 @@ require('NeoColumn').setup(
 
 require('glow').setup()
 
+local wk = require("which-key")
+wk.register({
+    l = {
+        l = { function()
+            require('hlchunk').setup({
+                indent = {
+                    chars = { "│", "¦", "┆", "┊", }, -- more code can be found in https://unicodeplus.com/
+                    style = { "#5E81AC" },
+                },
+                blank = {
+                    enable = false,
+                }
+            })
+        end, "hlchunk" },
+        h = { "<cmd>DisableHLChunk<cr>", "Enable HLChunk" }
+    },
+}, { prefix = "<leader>" })
+
+wk.setup {}
+
 vim.keymap.set("n", "<leader>xx", function() require("trouble").open() end)
 vim.keymap.set("n", "<leader>xw", function() require("trouble").open("workspace_diagnostics") end)
 vim.keymap.set("n", "<leader>xd", function() require("trouble").open("document_diagnostics") end)
 vim.keymap.set("n", "<leader>xq", function() require("trouble").open("quickfix") end)
 vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end)
 vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end)
-
-require('hlchunk').setup({
-    indent = {
-        chars = { "│", "¦", "┆", "┊", }, -- more code can be found in https://unicodeplus.com/
-        style = { "#5E81AC" },
-    },
-    blank = {
-        enable = false,
-    }
-})
