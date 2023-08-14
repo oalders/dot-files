f7718e4acfb3534a2f6b3ec1ad0f0cc58096790c
Olaf Alders <olaf@wundersolutions.com>
2023-08-14 11:38:39 -0400

    Use a shorter variable for --in

--- installer/ubi.sh
+++ installer/ubi.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-install_dir="$HOME/local/bin"
-mkdir -p "$install_dir"
+in="$HOME/local/bin"
+mkdir -p "$in"
 
 if [[ ! "$(command -v curl)" && "$(command -v apt-get)" ]]; then
     if [[ ! "$(command -v sudo)" ]]; then
@@ -12,38 +12,38 @@ if [[ ! "$(command -v curl)" && "$(command -v apt-get)" ]]; then
     apt-get install curl --autoremove -y
 fi
 
-if [ ! "$(command -v "$install_dir/ubi")" ]; then
+if [ ! "$(command -v "$in/ubi")" ]; then
     curl --silent --location \
         https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
-        TARGET=$install_dir sh
+        TARGET=$in sh
 
 else
-    "$install_dir/ubi" --self-upgrade
+    "$in/ubi" --self-upgrade
 fi
 
-"$install_dir/ubi" --project oalders/is --in "$install_dir"
+"$in/ubi" --project oalders/is --in "$in"
 
 # shellcheck source=bash_functions.sh
 source ~/dot-files/bash_functions.sh
-add_path "$install_dir"
+add_path "$in"
 
 if ! is there omegasort; then
-    ubi --project houseabsolute/omegasort --in "$install_dir"
+    ubi --project houseabsolute/omegasort --in "$in"
 fi
 
 if ! is there precious; then
-    ubi --project houseabsolute/precious --in "$install_dir"
+    ubi --project houseabsolute/precious --in "$in"
 fi
 
 if is os name eq darwin; then
-    ubi --in "$install_dir" \
+    ubi --in "$in" \
         --url https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-x86_64-apple-darwin.tar.gz
 else
-    ubi --project sharkdp/bat --in "$install_dir"
+    ubi --project sharkdp/bat --in "$in"
 fi
 
-ubi --project cli/cli --in "$install_dir" --exe gh
-# ubi --project sharkdp/fd --in $install_dir
-# ubi --project Wilfred/difftastic --exe difft --in "$install_dir"
+ubi --project cli/cli --in "$in" --exe gh
+# ubi --project sharkdp/fd --in $in
+# ubi --project Wilfred/difftastic --exe difft --in "$in"
 
 exit
