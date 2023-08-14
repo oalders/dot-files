038c490b501cb0719bc67eabbe625d4a55a8d0aa
Olaf Alders <olaf@wundersolutions.com>
2023-08-14 11:37:40 -0400

    s/INSTALL_DIR/install_dir/ since this is not an env var

--- installer/ubi.sh
+++ installer/ubi.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-INSTALL_DIR="$HOME/local/bin"
-mkdir -p "$INSTALL_DIR"
+install_dir="$HOME/local/bin"
+mkdir -p "$install_dir"
 
 if [[ ! "$(command -v curl)" && "$(command -v apt-get)" ]]; then
     if [[ ! "$(command -v sudo)" ]]; then
@@ -12,38 +12,38 @@ if [[ ! "$(command -v curl)" && "$(command -v apt-get)" ]]; then
     apt-get install curl --autoremove -y
 fi
 
-if [ ! "$(command -v "$INSTALL_DIR/ubi")" ]; then
+if [ ! "$(command -v "$install_dir/ubi")" ]; then
     curl --silent --location \
         https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
-        TARGET=$INSTALL_DIR sh
+        TARGET=$install_dir sh
 
 else
-    "$INSTALL_DIR/ubi" --self-upgrade
+    "$install_dir/ubi" --self-upgrade
 fi
 
-"$INSTALL_DIR/ubi" --project oalders/is --in "$INSTALL_DIR"
+"$install_dir/ubi" --project oalders/is --in "$install_dir"
 
 # shellcheck source=bash_functions.sh
 source ~/dot-files/bash_functions.sh
-add_path "$INSTALL_DIR"
+add_path "$install_dir"
 
 if ! is there omegasort; then
-    ubi --project houseabsolute/omegasort --in "$INSTALL_DIR"
+    ubi --project houseabsolute/omegasort --in "$install_dir"
 fi
 
 if ! is there precious; then
-    ubi --project houseabsolute/precious --in "$INSTALL_DIR"
+    ubi --project houseabsolute/precious --in "$install_dir"
 fi
 
 if is os name eq darwin; then
-    ubi --in "$INSTALL_DIR" \
+    ubi --in "$install_dir" \
         --url https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-x86_64-apple-darwin.tar.gz
 else
-    ubi --project sharkdp/bat --in "$INSTALL_DIR"
+    ubi --project sharkdp/bat --in "$install_dir"
 fi
 
-ubi --project cli/cli --in "$INSTALL_DIR" --exe gh
-# ubi --project sharkdp/fd --in $INSTALL_DIR
-# ubi --project Wilfred/difftastic --exe difft --in "$INSTALL_DIR"
+ubi --project cli/cli --in "$install_dir" --exe gh
+# ubi --project sharkdp/fd --in $install_dir
+# ubi --project Wilfred/difftastic --exe difft --in "$install_dir"
 
 exit
