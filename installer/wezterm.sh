995a27f8619378736661dc568fb4258f5c08baaa
Olaf Alders <olaf@wundersolutions.com>
2023-09-17 19:14:38 -0400

    Simplify wezterm installer

--- installer/wezterm.sh
+++ installer/wezterm.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -e -u -o pipefail
-set -x
 
 install_for_linux() (
     sudo apt install -y libxcb-image0 libxkbcommon-x11-0
@@ -14,24 +13,15 @@ install_for_linux() (
     sudo dpkg -i "$file"
 )
 
-remove_wezterm() {
-    rm -rf /Applications/WezTerm.app \
-        /usr/local/bin/strip-ansi-escapes \
-        /usr/local/bin/wezterm \
-        /usr/local/bin/wezterm-gui \
-        /usr/local/bin/wezterm-mux-server
-}
-
 if is os name eq darwin; then
     if is there wezterm; then
         brew upgrade homebrew/cask/wezterm
     else
-        # remove_wezterm
         brew tap wez/wezterm
         brew install --cask wezterm --no-quarantine
     fi
 elif is os name eq linux; then
-    if [[ $IS_SUDOER == false ]]; then
+    if ! is user sudoer; then
         exit 0
     fi
     is there wezterm && is cli age wezterm lt 18 hours && exit
