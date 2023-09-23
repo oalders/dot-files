47364c2d93cd5da3a14c4d1d9f1777cfdc06c3bf
Olaf Alders <olaf@wundersolutions.com>
2023-09-23 15:18:58 +0000

    Hardcode golang version to install

--- installer/golang.sh
+++ installer/golang.sh
@@ -3,19 +3,19 @@
 # https://www.reddit.com/r/golang/comments/tfvb6i/comment/i0ye1r1/?utm_source=share&utm_medium=web2x&context=3
 
 cd /tmp || exit 1
-set -eux
+set -eu -o pipefail
 
-latest=$(curl https://go.dev/VERSION?m=text)
-echo "version $latest"
+version=1.21.1
 
-arch="amd64"
+arch="$(is known arch)"
 
 if [[ $(uname -m) == "armv7l" ]]; then # raspberry pi
     arch="armv6l"
 fi
 
-filename="$latest".linux-"$arch".tar.gz
+filename="go$version.linux-$arch.tar.gz"
+url="https://go.dev/dl/$filename"
 
-curl --location -O "https://go.dev/dl/$filename"
+curl --location -O $url
 sudo rm -rf /usr/local/go/
 sudo tar -C /usr/local -xzf "$filename"
