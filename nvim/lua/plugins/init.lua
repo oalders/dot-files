d9641760ba1b24e16fda5d6bd85946c54c44e061
Olaf Alders <olaf@wundersolutions.com>
2023-10-20 17:45:24 +0000

    Tidy nvim/lua/plugins/init.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -277,15 +277,15 @@ navbuddy.setup {
 }
 
 require 'lspconfig'.gopls.setup({
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
-      gofumpt = true,
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+        },
     },
-  },
 })
 require 'lspconfig'.golangci_lint_ls.setup {}
 
