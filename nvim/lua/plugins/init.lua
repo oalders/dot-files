00683932d893f3bf3f7c43bd5c3138dd35a693ee
Olaf Alders <olaf@wundersolutions.com>
2023-10-20 17:43:41 +0000

    Tweak copilot suggestion appearance

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -158,6 +158,14 @@ cmp.setup.cmdline(':', {
     },
 })
 
+cmp.event:on('menu_opened', function()
+    vim.b.copilot_suggestion_hidden = true
+end)
+
+cmp.event:on('menu_closed', function()
+    vim.b.copilot_suggestion_hidden = false
+end)
+
 -- Set up lspconfig.
 -- Mappings.
 vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
