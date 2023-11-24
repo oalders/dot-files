20e0a587fece3eae09a05b59fa685b8e8c96f8bb
Olaf Alders <olaf@wundersolutions.com>
2023-11-24 18:12:19 +0000

    Move noice config to a new file

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -341,34 +341,6 @@ require('lspconfig').pylsp.setup {
 wildchar = "<tab>"
 -- require("lspconfig").rust_analyzer.setup {}
 
-require("noice").setup({
-    lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-        },
-        progress = {
-            enabled = true,
-            format = 'lsp_progress',
-            format_done = 'lsp_progress_done',
-            view = 'mini',
-        },
-    },
-    messages = {
-        enabled = false,
-    },
-    -- you can enable a preset for easier configuration
-    presets = {
-        bottom_search = false,        -- use a classic bottom cmdline for search
-        command_palette = true,       -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,       -- add a border to hover docs and signature help
-    },
-})
-
 require('other-nvim').setup {
     mappings = { 'golang' }
 }
@@ -438,4 +410,5 @@ require('ufo').setup({
 
 require('conf/lualine')
 require('conf/mason')
+require('conf/noice')
 require('conf/which-key')
