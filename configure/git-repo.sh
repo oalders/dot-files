f304fe29cfbbd5955139b2f0d05fb82b9993df30
Olaf Alders <olaf@wundersolutions.com>
2023-12-10 22:07:33 -0500

    Lower case bash variable which does not get exported

--- configure/git-repo.sh
+++ configure/git-repo.sh
@@ -5,19 +5,19 @@ set -eu
 # Set branch to $1 if it's defined, otherwise capture the output of a shell command
 branch=${1:-$(git symbolic-ref --short refs/remotes/origin/HEAD | cut -d'/' -f2)}
 
-if [ "$BRANCH" = "" ]; then
+if [ "$branch" = "" ]; then
     if [ -f ".git/refs/remotes/origin/main" ]; then
-        BRANCH="main"
+        branch="main"
     else
         echo "cannot find main branch name"
         exit 1
     fi
 fi
 
-echo "Setting main branch in local Git aliases to \"$BRANCH\""
+echo "Setting main branch in local Git aliases to \"$branch\""
 
-git config alias.dom "diff -w -M origin/$BRANCH...HEAD"
-git config alias.doms "diff -w -M origin/$BRANCH...HEAD --stat"
-git config alias.domo "diff -w -M origin/$BRANCH...HEAD --name-only"
-git config alias.from "!git fetch -p; git rebase origin/$BRANCH"
-git config alias.prom "pull --rebase origin $BRANCH"
+git config alias.dom "diff -w -M origin/$branch...HEAD"
+git config alias.doms "diff -w -M origin/$branch...HEAD --stat"
+git config alias.domo "diff -w -M origin/$branch...HEAD --name-only"
+git config alias.from "!git fetch -p; git rebase origin/$branch"
+git config alias.prom "pull --rebase origin $branch"
