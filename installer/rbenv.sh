eacf28c2cc946533bf41db0e14a2263ff37d5543
Olaf Alders <olaf@wundersolutions.com>
2024-02-11 13:13:53 -0500

    Ensure rbenv is installed and that a target version of ruby is available

--- installer/rbenv.sh
+++ installer/rbenv.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
-# Ruby installer
-
 set -eu -o pipefail
+version=3.3.0
 
-if is os name ne darwin; then
-    exit 0
+if is os name ne 'darwin'; then
+    exit
 fi
 
-brew install rbenv
+if ! is there rbenv; then
+    brew install rbenv
+fi
 
-exit 0
+# Might need to initialize rbenv in the shell
+if [ -z "${RBENV_SHELL:-}" ]; then
+    eval "$(rbenv init - bash)"
+fi
+
+if is cli output stdout rbenv --arg version like "^$version\b"; then
+    echo "Ruby version $version is already installed"
+    exit
+fi
+
+if ! is cli output stdout rbenv --arg versions like "\b$version\b" --debug; then
+    rbenv install $version
+fi
+
+rbenv global $version
+
+if ! is cli version ruby eq $version; then
+    echo "Ruby version $version is not available"
+fi
