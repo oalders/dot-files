85efd7c290f6a657c1d4fa7ee085b4df03666966
Olaf Alders <olaf@wundersolutions.com>
2024-02-13 14:14:25 -0500

    Let CoPilot refactor install.sh

--- install.sh
+++ install.sh
@@ -16,45 +16,55 @@ run_installer() {
     echo ""
 }
 
+run_installers() {
+    local installers=("$@")
+    for installer in "${installers[@]}"; do
+        run_installer "$installer"
+    done
+}
+
 add_path "$HOME/local/bin"
 
-if is os name eq darwin; then
-    MAC_INSTALLERS=(
+run_mac_installers() {
+    local mac_installers=(
         ./installer/xcode.sh
         ./installer/homebrew.sh
         # time ./installer/homebrew-maintenance.sh || true
         # ./installer/fonts.sh
-
         # https://github.com/kcrawford/dockutil/issues/127
         # ./installer/dockutil.sh
         #./configure/dock.sh
         ./installer/lua.sh
     )
 
-    for f in "${MAC_INSTALLERS[@]}"; do
-        run_installer "$f"
-    done
+    run_installers "${mac_installers[@]}"
+}
+
+run_general_installers() {
+    local installers=(
+        ./installer/linux.sh
+        ./installer/maintenance.sh
+        ./installer/wezterm.sh
+        ./configure/git.sh
+        ./configure/ssh.sh
+        ./installer/pip.sh
+        ./installer/nvim.sh
+        ./configure/vim.sh
+        ./configure/tmux.sh
+        ./installer/npm.sh
+        ./installer/cpan.sh
+        ./installer/cargo.sh
+        ./installer/cz.sh
+        ./installer/oh-my-posh.sh
+    )
+
+    run_installers "${installers[@]}"
+}
+
+if is os name eq darwin; then
+    run_mac_installers
 fi
 
-INSTALLERS=(
-    ./installer/linux.sh
-    ./installer/maintenance.sh
-    ./installer/wezterm.sh
-    ./configure/git.sh
-    ./configure/ssh.sh
-    ./installer/pip.sh
-    ./installer/nvim.sh
-    ./configure/vim.sh
-    ./configure/tmux.sh
-    ./installer/npm.sh
-    ./installer/cpan.sh
-    ./installer/cargo.sh
-    ./installer/cz.sh
-    ./installer/oh-my-posh.sh
-)
-
-for f in "${INSTALLERS[@]}"; do
-    run_installer "$f"
-done
+run_general_installers
 
 exit 0
