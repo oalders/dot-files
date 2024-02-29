d7124ff163e2fc4de2e6a64b417dec86ed391831
Olaf Alders <olaf@wundersolutions.com>
2024-02-29 11:16:51 -0500

    Don't try to reinstall VS Code

--- installer/vscode.sh
+++ installer/vscode.sh
@@ -6,7 +6,10 @@ if is os name ne darwin; then
     exit 0
 fi
 
-brew install visual-studio-code
+if ! is there code; then
+   brew install visual-studio-code
+fi
+
 open vscode:extension/vscodevim.vim
 
 exit 0
