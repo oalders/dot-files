71c8f9ffc0fa89c9fa45d3cb900351b77b9b01e5
Olaf Alders <olaf@wundersolutions.com>
2024-03-19 11:50:21 -0400

    Allow filenames to be passed to heic-to-jpeg.sh

--- bin/heic-to-jpeg.sh
+++ bin/heic-to-jpeg.sh
@@ -6,10 +6,18 @@ set -eu -x -o pipefail
 
 # formatOptions: low, normal, high or best
 
-find . -type f -iname '*.heic' | while read -r i; do \
-  fileNoExt="${i%.*}"; \
-  jpgFile="${fileNoExt}_heic_conv.jpg"; \
-  sips -s format jpeg -s formatOptions high "$i" --out "$jpgFile"; \
-  touch -r "$i" "$jpgFile"; \
-  rm "$i"; \
+if [ "$#" -eq 0 ]; then
+  # No arguments provided, find all .heic or .HEIC files
+  files=$(find . -type f \( -iname '*.heic' -o -iname '*.HEIC' \))
+else
+  # Arguments provided, use those as file names
+  files="$@"
+fi
+
+for i in $files; do
+  fileNoExt="${i%.*}"
+  jpgFile="${fileNoExt}_heic_conv.jpg"
+  sips -s format jpeg -s formatOptions high "$i" --out "$jpgFile"
+  touch -r "$i" "$jpgFile"
+  rm "$i"
 done
