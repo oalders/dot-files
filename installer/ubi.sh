6f08dc24a4adc9409d42ffbf47848d774ded97ee
Olaf Alders <olaf@wundersolutions.com>
2024-03-21 22:03:04 -0400

    Install kustomize

--- installer/ubi.sh
+++ installer/ubi.sh
@@ -42,6 +42,7 @@ maybe_install houseabsolute/precious
 maybe_install jqlang/jq
 maybe_install junegunn/fzf
 maybe_install oalders/is
+maybe_install kubernetes-sigs/kustomize
 
 if is cli output stdout hostname eq wolfblitzer; then
     ubi --url https://github.com/dandavison/delta/releases/download/0.16.5/git-delta_0.16.5_amd64.deb --in /tmp/ubi
