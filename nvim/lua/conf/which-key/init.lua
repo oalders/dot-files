a75b761af26757350f8e9d834404be9703b4841f
Olaf Alders <olaf@wundersolutions.com>
2024-04-03 12:55:00 -0400

    Move <leader>ot from vimscript to which-key

--- nvim/lua/conf/which-key/init.lua
+++ nvim/lua/conf/which-key/init.lua
@@ -1,21 +1,23 @@
 local tsj = require('treesj')
 local wk = require("which-key")
 _G.OpenThis = function(fname)
-    local cmd = 'ot --editor vim --print ' .. vim.fn.shellescape(fname)
+    local cmd = 'ot --editor nvim --json ' .. vim.fn.shellescape(fname)
     local handle = io.popen(cmd)
     local result = handle:read("*a")
     handle:close()
 
-    if result ~= "" then
-        vim.cmd('e ' .. result)  -- remove shellescape() here
+    local jsonResult = vim.fn.json_decode(result)
+
+    if jsonResult.success == true then
+        local editorArgs = table.concat(jsonResult.editor_args, " ")
+        vim.cmd('e ' .. editorArgs)
     else
-        print("Error: could not open file")
+        print("Error: " .. jsonResult.error .. (jsonResult.details and "Details: " .. jsonResult.details or ""))
     end
 
     -- Add the command to the command history
     vim.fn.histadd(':', string.format(':lua OpenThis("%s")', vim.fn.escape(fname, '"\\')))
 end
-
 wk.register({
     f = {
         f = { '<cmd>Files<cr>', 'open FZF file finder' },
