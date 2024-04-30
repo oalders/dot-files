c5ed95ea30608ae1421f9778e952e07ff2bbf443
Olaf Alders <olaf@wundersolutions.com>
2024-04-30 17:23:52 -0400

    Reformat chrome_tab_action()

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -16,37 +16,32 @@ end
 local function chrome_tab_action(url_substring, url_to_visit_if_tab_not_found)
     return applescript_action(
         [[
+        tell application "Google Chrome"
+            activate
 
-      tell application "Google Chrome"
-          activate
+            set foundWindow to false
+            set theTabIndex to -1
 
-          set foundWindow to false
-          set theTabIndex to -1
+            repeat with theWindow in every window
+                set theTabIndex to 0
+                repeat with theTab in every tab of theWindow
+                    set theTabIndex to theTabIndex + 1
+                    if theTab's URL contains "]] .. url_substring .. [[" then
+                        set foundWindow to theWindow
+                        set foundTabIndex to theTabIndex
+                    end if
+                end repeat
+            end repeat
 
-          repeat with theWindow in every window
-              set theTabIndex to 0
-              repeat with theTab in every tab of theWindow
-                  set theTabIndex to theTabIndex + 1
-                  if theTab's URL contains "]] ..
-        url_substring ..
-        [[" then
-                      set foundWindow to theWindow
-                      set foundTabIndex to theTabIndex
-                  end if
-              end repeat
-          end repeat
+            if foundWindow is not false then
+                set foundWindow's active tab index to foundTabIndex
+                set index of foundWindow to 1
+            else
+                open location "]] .. url_to_visit_if_tab_not_found .. [["
+            end if
 
-          if foundWindow is not false then
-              set foundWindow's active tab index to foundTabIndex
-              set index of foundWindow to 1
-          else
-              open location "]] ..
-        url_to_visit_if_tab_not_found .. [["
-          end if
-
-      end tell
-
-    ]]
+        end tell
+        ]]
     )
 end
 
