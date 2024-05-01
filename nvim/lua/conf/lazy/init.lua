37cddacb9d7311ceefeadd9b9332f94ba6fca31b
Olaf Alders <olaf@wundersolutions.com>
2024-05-01 22:51:32 +0000

    Add CopilotC-Nvim/CopilotChat.nvim

--- nvim/lua/conf/lazy/init.lua
+++ nvim/lua/conf/lazy/init.lua
@@ -87,6 +87,20 @@ require('lazy').setup({
     'windwp/nvim-autopairs', --
     'zbirenbaum/copilot.lua', -- GitHub Copilot
 
+    {
+        'CopilotC-Nvim/CopilotChat.nvim',
+        branch = 'canary',
+        dependencies = {
+          { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+          { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+        },
+        opts = {
+          debug = true, -- Enable debugging
+          -- See Configuration section for rest
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+    },
+
     -- folding
     'kevinhwang91/promise-async',
     'kevinhwang91/nvim-ufo',
