0bb9f0356b0255cb5fe4c411e29115c20d8aef1d
Olaf Alders <olaf@wundersolutions.com>
2024-05-16 11:44:20 -0400

    Don't resize jpegs twice

--- bin/resize-jpegs.sh
+++ bin/resize-jpegs.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-set -eu -x -o pipefail
+set -eu -o pipefail
 
 if [ "$#" -eq 0 ]; then
-    # No arguments provided, find all .jpg, .jpeg, .JPG or .JPEG files
-    files=$(find . -type f \( -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.JPG' -o -iname '*.JPEG' \))
+    # No arguments provided, find files
+    files=$(fd . -e jpg -e jpeg)
 else
     # Arguments provided, use those as file names
     files="$@"
 fi
 
+width=600
 for i in $files; do
+    newSuffix="-${width}.jpeg"
+if [[ $i == *$newSuffix ]]; then
+        continue
+    fi
     fileNoExt="${i%.*}"
-    resizedFile="${fileNoExt}-600.jpeg"
-    sips -Z 600 "$i" --out "$resizedFile"
-    touch -r "$i" "$resizedFile"
+    resizedFile="${fileNoExt}${newSuffix}"
+    sips -Z $width "$i" --out "$resizedFile"
 done
