e9eeb650894b1b2849d70b3166b0011b0583a571
Olaf Alders <olaf@wundersolutions.com>
2024-05-30 16:24:50 +0000

    Fix navigating between git hunks

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -54,6 +54,35 @@ require('conf/ufo')
 require('conf/open-this') -- needs to happen before which-key
 require('conf/which-key')
 
-require('gitsigns').setup({ linehl = true, numhl = true, word_diff = true })
+require('gitsigns').setup({
+    linehl = true,
+    numhl = true,
+    word_diff = true,
+    on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({ ']c', bang = true })
+            else
+                gitsigns.nav_hunk('next')
+            end
+        end)
+
+        map('n', '[c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({ '[c', bang = true })
+            else
+                gitsigns.nav_hunk('prev')
+            end
+        end)
+    end,
+})
 require('persisted').setup({})
 require('yankbank').setup()
