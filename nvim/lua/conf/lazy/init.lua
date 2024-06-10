b01e0d941aea37df60e02188267054c39401c85c
Olaf Alders <olaf@wundersolutions.com>
2024-06-10 14:56:07 +0000

    Add tris203/precognition.nvim

--- nvim/lua/conf/lazy/init.lua
+++ nvim/lua/conf/lazy/init.lua
@@ -291,5 +291,33 @@ require('lazy').setup({
     },
     { 'akinsho/toggleterm.nvim', version = '*', config = true },
 
+    {
+        'tris203/precognition.nvim',
+        event = 'VeryLazy',
+        config = {
+            startVisible = true,
+            showBlankVirtLine = true,
+            highlightColor = { link = 'Comment' },
+            hints = {
+                Caret = { text = '^', prio = 2 },
+                Dollar = { text = '$', prio = 1 },
+                MatchingPair = { text = '%', prio = 5 },
+                Zero = { text = '0', prio = 1 },
+                w = { text = 'w', prio = 10 },
+                b = { text = 'b', prio = 9 },
+                e = { text = 'e', prio = 8 },
+                W = { text = 'W', prio = 7 },
+                B = { text = 'B', prio = 6 },
+                E = { text = 'E', prio = 5 },
+            },
+            gutterHints = {
+                G = { text = 'G', prio = 10 },
+                gg = { text = 'gg', prio = 9 },
+                PrevParagraph = { text = '{', prio = 8 },
+                NextParagraph = { text = '}', prio = 8 },
+            },
+        },
+    },
+
     'tpope/vim-sensible', -- Defaults everyone can agree on
 })
