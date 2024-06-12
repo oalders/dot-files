f75154e56f7a30a98546769f29751cddbf8a8bfa
Olaf Alders <olaf@wundersolutions.com>
2024-06-12 13:51:40 -0400

    debounce ubi

--- installer/ubi.sh
+++ installer/ubi.sh
@@ -2,24 +2,17 @@
 
 set -eu
 
-cache_dir=~/.cache/is
-debounce="$cache_dir"/debounce-ubi.txt
-debounce_hours=6
 in="$HOME/local/bin"
-
-if [ -e $debounce ] && [ -e "$in/is" ] && is cli version is gte 0.5 && is fso age $debounce lt $debounce_hours h; then
-    echo "🚥 will not run more than once every $debounce_hours hours"
-    exit
-fi
 
 # shellcheck source=bash_functions.sh
 source ~/dot-files/bash_functions.sh
 
-mkdir -p "$in"
+test -d "$in" || mkdir -p "$in"
 add_path "$in"
 
-set -x
+# set -x
 
+# can't use "is" here as we may not yet have it
 if [[ ! "$(command -v curl)" && "$(command -v apt-get)" ]]; then
     if [[ ! "$(command -v sudo)" ]]; then
         apt-get update && apt-get install sudo --autoremove -y
@@ -27,28 +20,27 @@ if [[ ! "$(command -v curl)" && "$(command -v apt-get)" ]]; then
     apt-get install curl --autoremove -y
 fi
 
-if [ ! "$(command -v "$in/ubi")" ]; then
+maybe_install() {
+    project="$1"
+    shift
+    debounce 1 d ubi --project "$project" --in "$in" "$@"
+}
+
+if [ ! -f "$in/ubi" ]; then
     curl --silent --location \
         https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
         TARGET=$in sh
 
 else
-    if is cli age "$in/ubi" gt 7 days; then
-        "$in/ubi" --self-upgrade
-    fi
+    debounce 1 d "$in/ubi" --self-upgrade
 fi
 
-maybe_install() {
-    local project
-    local repo
-    IFS='/' read -r project repo <<<"$1"
-    if ! is there "$repo" || is cli age "$repo" gt 7 days; then
-        shift
-        ubi --project "$project/$repo" --in "$in" "$@"
-    fi
-}
+if [[ ! -f "$in/is" ]]; then
+    ubi --project oalders/is --in "$in"
+else
+    maybe_install oalders/is
+fi
 
-maybe_install oalders/is
 maybe_install crate-ci/typos
 maybe_install houseabsolute/omegasort
 maybe_install houseabsolute/precious
@@ -71,21 +63,15 @@ if is os name eq darwin; then
             --url https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-x86_64-apple-darwin.tar.gz
     fi
 else
-    ubi --project sharkdp/bat --in "$in"
+    maybe_install sharkdp/bat
 fi
 
-if ! is there gh || is cli age gh gt 7 days; then
-    ubi --project cli/cli --in "$in" --exe gh
-fi
+maybe_install cli/cli --exe gh
 
-if is there gh && ! gh extension list | grep copilot; then
+if is there gh && ! gh extension list | grep --quiet copilot; then
     gh extension install github/gh-copilot || true
 fi
 
-if is there gh && ! gh extension list | grep gh-dash; then
+if is there gh && ! gh extension list | grep --quiet gh-dash; then
     gh extension install dlvhdr/gh-dash || true
 fi
-
-mkdir -p $cache_dir
-touch $debounce
-exit
