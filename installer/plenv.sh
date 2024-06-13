aef5d968bc54020cbad84592ff9faf1b8a0943cc
Olaf Alders <olaf@wundersolutions.com>
2024-06-13 09:58:17 -0400

    Bump plenv default install version to 5.40.0

--- installer/plenv.sh
+++ installer/plenv.sh
@@ -21,7 +21,7 @@ else
     git clone https://github.com/tokuhirom/Perl-Build.git "$my_perl_build_dir"
 fi
 
-perl_version=5.38.0
+perl_version=5.40.0
 
 plenv install $perl_version
 plenv global $perl_version
