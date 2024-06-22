ff08de923562275572e831287eba8a440ed67cb0
Olaf Alders <olaf@wundersolutions.com>
2024-06-22 15:59:37 -0400

    debounce Alfred workflow installers (and upgrade emoji and browser tabs

--- installer/alfred-workflows.sh
+++ installer/alfred-workflows.sh
@@ -4,7 +4,11 @@ set -eux
 
 cd /tmp
 
+# shellcheck source=bash_functions.sh
+source ~/dot-files/bash_functions.sh
+
 # Alfred won't let you add multiple workflows at once
+# shellcheck disable=SC2317
 wait_for_input() {
     url=$1
     file=$2
@@ -16,18 +20,23 @@ wait_for_input() {
     read -n 1 -s -r -p "Press any key to continue"
 }
 
-wait_for_input 'https://github.com/alexchantastic/alfred-ip-address-workflow/releases/download/v1.0.3' 'alfred-ip-address-workflow.alfredworkflow'
-wait_for_input 'https://github.com/epilande/alfred-browser-tabs/releases/download/v1.0.5' 'Browser-Tabs.alfredworkflow'
-wait_for_input 'https://github.com/gharlan/alfred-github-workflow/releases/download/v1.7.1' 'github.alfredworkflow'
-wait_for_input 'https://github.com/jsumners/alfred-emoji/releases/download/v1.12.0' 'alfred-emoji.alfredworkflow'
-wait_for_input 'https://github.com/mrodalgaard/alfred-network-workflow/releases/download/v1.1' 'Network.alfredworkflow'
-wait_for_input 'https://github.com/packal/repository/raw/master/com.pawelgrzybek.div' 'div.alfredworkflow'
-wait_for_input 'https://github.com/ruedap/alfred-font-awesome-workflow/releases/download/v5.15.3.1' 'Font-Awesome.alfredworkflow'
+debounce 90 d wait_for_input 'https://github.com/alexchantastic/alfred-ip-address-workflow/releases/download/v1.0.3' 'alfred-ip-address-workflow.alfredworkflow'
+debounce 90 d wait_for_input 'https://github.com/epilande/alfred-browser-tabs/releases/download/v1.0.7' 'Browser-Tabs.alfredworkflow'
+debounce 90 d wait_for_input 'https://github.com/gharlan/alfred-github-workflow/releases/download/v1.7.1' 'github.alfredworkflow'
+debounce 90 d wait_for_input 'https://github.com/jsumners/alfred-emoji/releases/download/v2.2.0' 'alfred-emoji.alfredworkflow'
+debounce 90 d wait_for_input 'https://github.com/mrodalgaard/alfred-network-workflow/releases/download/v1.1' 'Network.alfredworkflow'
+debounce 90 d wait_for_input 'https://github.com/packal/repository/raw/master/com.pawelgrzybek.div' 'div.alfredworkflow'
+debounce 90 d wait_for_input 'https://github.com/ruedap/alfred-font-awesome-workflow/releases/download/v5.15.3.1' 'Font-Awesome.alfredworkflow'
 
-repo=alfred-metacpan-workflow
-rm -rf $repo
-git clone "git@github.com:oalders/$repo.git"
-cd $repo && mkdir -p dist && make && open dist/metacpan-0.0.5.alfredworkflow
+# shellcheck disable=SC2317
+alfred_metacpan() {
+    repo=alfred-metacpan-workflow
+    rm -rf "$repo"
+    git clone "git@github.com:oalders/$repo.git"
+    cd "$repo" && mkdir -p dist && make && open dist/metacpan-0.0.5.alfredworkflow
+}
+
+debounce 90 d alfred_metacpan
 
 exit 0
 
