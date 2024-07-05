ce248a1071de8230970e9b38acb58a6f92bee69e
Olaf Alders <olaf@wundersolutions.com>
2024-07-05 16:20:01 -0400

    tidy lua

--- nvim/lua/conf/lspconfig/init.lua
+++ nvim/lua/conf/lspconfig/init.lua
@@ -1,6 +1,9 @@
 -- vim.lsp.set_log_level("debug")
 
-require('lspsaga').setup({ code_action = { extend_gitsigns = true }, code_action_prompt = false })
+require('lspsaga').setup({
+    code_action = { extend_gitsigns = true },
+    code_action_prompt = false,
+})
 
 -- reduce left-right jitter when signcolumn appears and disappears while
 -- scrolling vertically.
