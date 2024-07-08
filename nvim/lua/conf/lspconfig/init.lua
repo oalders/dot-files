098ccf58a436109491be57e710be1e8329ed1bcc
Olaf Alders <olaf@wundersolutions.com>
2024-07-08 20:22:38 +0000

    Configure JS and TS inlay hints

--- nvim/lua/conf/lspconfig/init.lua
+++ nvim/lua/conf/lspconfig/init.lua
@@ -51,7 +51,34 @@ lspconfig.lua_ls.setup({
         },
     },
 })
-lspconfig.tsserver.setup({})
+lspconfig.tsserver.setup({
+    settings = {
+        typescript = {
+            inlayHints = {
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            },
+        },
+        javascript = {
+            inlayHints = {
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            },
+        },
+    },
+})
 lspconfig.yamlls.setup({})
 
 -- Set up lspconfig.
