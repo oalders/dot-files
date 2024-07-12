31d5bf522a709435b1ca1ee894320cb01d6fd0c1
Olaf Alders <olaf@wundersolutions.com>
2024-07-12 15:09:51 +0000

    Add mvllow/modes.nvim

--- nvim/lua/conf/lazy/init.lua
+++ nvim/lua/conf/lazy/init.lua
@@ -334,6 +334,37 @@ require('lazy').setup({
         end,
     },
 
+    {
+        'mvllow/modes.nvim',
+        tag = 'v0.2.1',
+        opts = {
+            colors = {
+                bg = '', -- Optional bg param, defaults to Normal hl group
+                copy = '#f5c359',
+                delete = '#c75c6a',
+                insert = '#78ccc5',
+                visual = '#9745be',
+            },
+
+            -- Set opacity for cursorline and number background
+            line_opacity = 0.15,
+
+            -- Enable cursor highlights
+            set_cursor = true,
+
+            -- Enable cursorline initially, and disable cursorline for inactive windows
+            -- or ignored filetypes
+            set_cursorline = true,
+
+            -- Enable line number highlights to match cursorline
+            set_number = true,
+
+            -- Disable modes highlights in specified filetypes
+            -- Please PR commonly ignored filetypes
+            ignore_filetypes = { 'NvimTree', 'TelescopePrompt' },
+        },
+    },
+
     -- neotest
     {
         'nvim-neotest/neotest',
