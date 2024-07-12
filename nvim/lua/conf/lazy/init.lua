a14736e3bf7f2e4e0ad6c0a8cdadd43ac98b6244
Olaf Alders <olaf@wundersolutions.com>
2024-07-12 23:00:08 +0000

    Experiment with minimap

--- nvim/lua/conf/lazy/init.lua
+++ nvim/lua/conf/lazy/init.lua
@@ -368,6 +368,19 @@ require('lazy').setup({
         },
     },
 
+    {
+        'Isrothy/neominimap.nvim',
+        enabled = true,
+        lazy = false,                  -- WARN: NO NEED to Lazy load
+        init = function()
+            vim.opt.wrap = false       -- Recommended
+            vim.opt.sidescrolloff = 36 -- It's recommended to set a large value
+            vim.g.neominimap = {
+                auto_enable = true,
+            }
+        end,
+    },
+
     -- neotest
     {
         'nvim-neotest/neotest',
