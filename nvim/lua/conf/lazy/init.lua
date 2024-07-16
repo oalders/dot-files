a4ee3d18631c7e9b50cb448fb6987f34d5b25a18
Olaf Alders <olaf@wundersolutions.com>
2024-07-16 22:00:58 +0000

    Revert mapleader and maplocalleader overrides from lazy.nvim default setup

--- nvim/lua/conf/lazy/init.lua
+++ nvim/lua/conf/lazy/init.lua
@@ -25,8 +25,8 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
+-- vim.g.mapleader = ' '
+-- vim.g.maplocalleader = '\\'
 
 require('lazy').setup({
     spec = {
