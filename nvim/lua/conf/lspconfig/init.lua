b06106db6fcfc060d93a6db48adb8c24c56f578d
Olaf Alders <olaf@wundersolutions.com>
2024-09-20 20:35:04 +0000

    Don't use deprecated vim.diagnostic APIs

--- nvim/lua/conf/lspconfig/init.lua
+++ nvim/lua/conf/lspconfig/init.lua
@@ -90,8 +90,8 @@ lspconfig.yamlls.setup({})
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = true }) end)
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = true }) end)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 local no_inlay_hints = { '' }
