3d735755d5c27ef2cda17fa654d7024bf35e941e
Olaf Alders <olaf@wundersolutions.com>
2024-09-23 14:30:16 -0400

    maybe_install lazydocker

--- installer/ubi.sh
+++ installer/ubi.sh
@@ -43,6 +43,7 @@ maybe_install bensadeh/tailspin --exe tspin
 maybe_install crate-ci/typos
 maybe_install houseabsolute/omegasort
 maybe_install houseabsolute/precious
+maybe_install jesseduffield/lazydocker
 maybe_install jqlang/jq
 maybe_install junegunn/fzf
 maybe_install kubernetes-sigs/kustomize
