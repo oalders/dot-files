697f1cb982c9220d7aca1596864487fc0430bab4
Olaf Alders <olaf@wundersolutions.com>
2024-10-30 13:38:23 -0400

    Simplify bat theme setup

--- configure/bat.sh
+++ configure/bat.sh
@@ -3,7 +3,7 @@
 set -eux -o pipefail
 
 if bat --list-themes | grep -q tokyonight; then
-    echo "'tokyonight' themes already exist"
+    # tokyonight themes already exist
     exit 0
 fi
 
@@ -12,7 +12,10 @@ themes_dir="$(bat --config-dir)/themes"
 mkdir -p "$themes_dir"
 cd "$themes_dir"
 
+# choose from day, night, moon or storm
+flavour=moon
+
 # Replace _moon in the lines below with _day, _night, or _storm if needed.
-curl -O https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_moon.tmTheme
+curl -O "https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_${flavour}.tmTheme"
 bat cache --build
 bat --list-themes | grep tokyo # should output "tokyonight_moon"
