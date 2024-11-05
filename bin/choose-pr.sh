41e308a8bae16352f9d7f098324e83e12578768b
Olaf Alders <olaf@wundersolutions.com>
2024-11-05 13:55:14 -0500

    Allow for author and label args to be provided to choose-pr.sh

--- bin/choose-pr.sh
+++ bin/choose-pr.sh
@@ -2,8 +2,16 @@
 
 set -eu -o pipefail
 
+author=${1:-"app/dependabot"}
+label=${2:-""}
+
+gh_command="gh pr list --author \"$author\" --json headRefName"
+if [ -n "$label" ]; then
+    gh_command="$gh_command --label \"$label\""
+fi
+
 # xargs requires -o because we are running an interactive application
-gh pr list --author "app/dependabot" --json headRefName |
+eval "$gh_command" |
     jq '.[].headRefName' |
     fzf |
     xargs -n 1 -o approve-lockfile-pr.sh origin
