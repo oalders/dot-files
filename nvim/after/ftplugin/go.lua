6a19a0010f69e9ba0fb28604e496cb6b4ca9be45
Olaf Alders <olaf@wundersolutions.com>
2024-11-15 18:02:37 +0000

    Don't show tabs in Golang files

---
+++ nvim/after/ftplugin/go.lua
@@ -0,0 +1,5 @@
+vim.opt_local.expandtab = false  -- Use tabs instead of spaces
+vim.opt_local.shiftwidth = 4     -- Number of spaces to use for each step of (auto)indent
+vim.opt_local.tabstop = 4        -- Number of spaces that a <Tab> in the file counts for
+vim.opt_local.softtabstop = 4    -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt_local.list = false       -- Do not display tab markers
