d7dd49e61dd221fc075a6b37e2e92ede6aea3f2f
Olaf Alders <olaf@wundersolutions.com>
2025-01-14 17:00:14 +0000

    Fix term config on linux

--- configure/term.sh
+++ configure/term.sh
@@ -8,8 +8,13 @@ infocmp -l -x | grep Smulx && exit
 
 infocmp > $TERMINFO_FILE
 
+if is os name eq darwin; then
 sed -i '' '/smul=\\E\[4m,/a\
 	Smulx=\\E\[4:%p1%dm,' "$TERMINFO_FILE"
+else
+sed -i '/smul=\\E\[4m,/a\
+ 	Smulx=\\E\[4:%p1%dm,' "$TERMINFO_FILE"
+fi
 if ! tic -x "$TERMINFO_FILE"; then
     echo "Error: Failed to compile terminfo file"
 fi
