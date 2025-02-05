aa976997015cb8843bde23f3ec8e068d82ec489a
Olaf Alders <olaf@wundersolutions.com>
2025-02-05 11:30:25 -0500

    Add a microphone plugin to sketchybar

--- installer/sketchybar.sh
+++ installer/sketchybar.sh
@@ -8,4 +8,7 @@ brew install sketchybar
 ln -s ~/dot-files/sketchybar ~/.config/
 brew install --cask font-hack-nerd-font
 
+# for getting inpuc mic name
+brew install switchaudio-osx
+
 osascript -e 'tell application "System Events" to set autohide menu bar of dock preferences to true'
---
+++ sketchybar/colors.sh
@@ -0,0 +1,27 @@
+# Tokyo Night Color Palette
+export BLACK=0xff1A1B26
+export WHITE=0xffC6D0F5
+export RED=0xffF7768E
+export GREEN=0xff9ECE6A
+export BLUE=0xff7AA2F7
+export YELLOW=0xffE0AF68
+export ORANGE=0xffFF9E64
+export MAGENTA=0xffBB9AF7
+export GREY=0xffA9B1D6
+export CYAN=0xff7DCFFF
+export TRANSPARENT=0x00000000
+export BG0=0xff1A1B26
+export BG1=0xff16161E
+export BG2=0xff2F3549
+
+# General bar colors
+export BAR_COLOR=$BG0
+export BAR_BORDER_COLOR=$BG2
+export BACKGROUND_0=$BG0
+export BACKGROUND_1=$BG1
+export BACKGROUND_2=$BG2
+export ICON_COLOR=$WHITE  # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export POPUP_BACKGROUND_COLOR=$BAR_COLOR
+export POPUP_BORDER_COLOR=$WHITE
+export SHADOW_COLOR=$BLACK
