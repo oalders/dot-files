6ad21f7f490d5e6c7a0da1890268326746d6f03d
Olaf Alders <olaf@wundersolutions.com>
2025-02-05 11:03:45 -0500

    set menu bar to autohide at the command line

--- installer/sketchybar.sh
+++ installer/sketchybar.sh
@@ -7,3 +7,5 @@ brew install sketchybar
 
 ln -s ~/dot-files/sketchybar ~/.config/
 brew install --cask font-hack-nerd-font
+
+osascript -e 'tell application "System Events" to set autohide menu bar of dock preferences to true'
