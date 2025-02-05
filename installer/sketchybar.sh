88bb2b3f3814498b3a205ed0dea4ffa681bc81c0
Olaf Alders <olaf@wundersolutions.com>
2025-02-05 14:12:49 -0500

    Start sketchybar automatically

--- installer/sketchybar.sh
+++ installer/sketchybar.sh
@@ -8,7 +8,9 @@ brew install sketchybar
 ln -s ~/dot-files/sketchybar ~/.config/
 brew install --cask font-hack-nerd-font
 
-# for getting inpuc mic name
+# for getting input mic name
 brew install switchaudio-osx
+
+brew services start sketchybar
 
 osascript -e 'tell application "System Events" to set autohide menu bar of dock preferences to true'
