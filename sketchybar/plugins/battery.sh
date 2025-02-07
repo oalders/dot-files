193fe27cdb5b3614525fd4d28f79bed4d6f2bea9
Olaf Alders <olaf@wundersolutions.com>
2025-02-07 00:14:03 -0500

    Change battery colour based on charge remaining

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -7,14 +7,16 @@ if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
+. "$CONFIG_DIR/colors.sh"
+
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON="" ICON_COLOR=$GREEN
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON="" ICON_COLOR=$WHITE
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON="" ICON_COLOR=$YELLOW
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON="" ICON_COLOR=$RED
   ;;
   *) ICON=""
 esac
@@ -25,4 +27,4 @@ fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%" icon.color="$ICON_COLOR"
