a2185360008274534cc9d1cfc742b43a8ad7ca91
Olaf Alders <olaf@wundersolutions.com>
2025-02-07 18:03:39 -0500

    Add wifi to sketchybar

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
 
-update() {
-    source "$CONFIG_DIR/icons.sh"
-    SSID="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: ' '/ SSID: / {print $2}')"
-    IP="$(ipconfig getifaddr en0)"
-
-    ICON="$([ -n "$IP" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
-    LABEL="$([ -n "$IP" ] && echo "$SSID ($IP)" || echo "Disconnected")"
-
-    sketchybar --set $NAME icon="$ICON" label="$LABEL"
-}
-
-click() {
-    CURRENT_WIDTH="$(sketchybar --query $NAME | jq -r .label.width)"
-
-    WIDTH=0
-    if [ "$CURRENT_WIDTH" -eq "0" ]; then
-        WIDTH=dynamic
-    fi
-
-    sketchybar --animate sin 20 --set $NAME label.width="$WIDTH"
-}
-
-case "$SENDER" in
-"wifi_change")
-    update
-    ;;
-"mouse.clicked")
-    click
-    ;;
-esac
+# CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
+SSID="$(ipconfig getsummary "$(networksetup -listallhardwareports | awk '/Wi-Fi|AirPort/{getline; print $NF}')" | grep '  SSID : ' | awk -F ': ' '{print $2}')"
+NAME=wifi
+if [ "$SSID" = "" ]; then
+  sketchybar -m --set $NAME label="😭" icon=󰤭
+else
+  sketchybar -m --set $NAME label="$SSID" icon=
+fi
