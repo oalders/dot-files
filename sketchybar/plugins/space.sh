c90338735f0d73e6b44b88465d5c187cba7c194a
Olaf Alders <olaf@wundersolutions.com>
2025-03-20 16:23:24 -0400

    Ensure currently in-focus space is highlighted with a button

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -4,4 +4,7 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
+
+# $NAME will be space.1, space.2, space.3, etc
+# $SELECTED is "true" or "false"
 sketchybar --set "$NAME" background.drawing="$SELECTED"
