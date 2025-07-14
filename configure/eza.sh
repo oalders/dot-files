93ad2faf9d2822ee29b148f30172451759b67128
Olaf Alders <olaf@wundersolutions.com>
2025-07-14 22:33:00 +0000

    tidy configure/eza.sh

--- configure/eza.sh
+++ configure/eza.sh
@@ -7,4 +7,4 @@ mkdir -p ~/.config/eza/themes
 
 # Download the Tokyo Night theme
 curl -o ~/.config/eza/theme.yml \
-  https://raw.githubusercontent.com/eza-community/eza-themes/main/themes/tokyonight.yml
+    https://raw.githubusercontent.com/eza-community/eza-themes/main/themes/tokyonight.yml
