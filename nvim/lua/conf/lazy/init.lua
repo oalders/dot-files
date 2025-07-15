04ca1498388b1a07a872024ee8cfd1c5ef299933
Olaf Alders <olaf@wundersolutions.com>
2025-07-15 21:23:51 +0000

    Replace noice with snacks

--- nvim/lua/conf/lazy/init.lua
+++ nvim/lua/conf/lazy/init.lua
@@ -80,6 +80,10 @@ require('lazy').setup({
                         },
                     },
                 },
+                notifier = { enabled = true },
+                input = { enabled = true },
+                terminal = { enabled = true },
+                toggle = {},
             },
         },
         {
@@ -1121,46 +1125,6 @@ require('lazy').setup({
             },
         },
 
-        -- noice
-        {
-            'folke/noice.nvim',
-            event = 'VeryLazy',
-            opts = {
-                lsp = {
-                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                    override = {
-                        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                        ['vim.lsp.util.stylize_markdown'] = true,
-                        ['cmp.entry.get_documentation'] = true,
-                    },
-                    progress = {
-                        enabled = true,
-                        format = 'lsp_progress',
-                        format_done = 'lsp_progress_done',
-                        view = 'mini',
-                    },
-                },
-                messages = {
-                    enabled = false,
-                },
-                -- you can enable a preset for easier configuration
-                presets = {
-                    bottom_search = false, -- use a classic bottom cmdline for search
-                    command_palette = true, -- position the cmdline and popupmenu together
-                    long_message_to_split = true, -- long messages will be sent to a split
-                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                    lsp_doc_border = true, -- add a border to hover docs and signature help
-                },
-            },
-            dependencies = {
-                -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-                'MunifTanjim/nui.nvim',
-                -- OPTIONAL:
-                --   `nvim-notify` is only needed, if you want to use the notification view.
-                --   If not available, we use `mini` as the fallback
-                'rcarriga/nvim-notify',
-            },
-        },
         {
             'akinsho/toggleterm.nvim',
             version = '*',
