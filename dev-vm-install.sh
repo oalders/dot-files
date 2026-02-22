74ef5b0f2a38557a22edd7429b0f6ec7c433bcbb
Olaf Alders <olaf@wundersolutions.com>
2026-02-22 14:35:09 +0000

    simplify dev-vm-install.sh

--- dev-vm-install.sh
+++ dev-vm-install.sh
@@ -3,25 +3,31 @@
 set -euxo pipefail
 
 in=~/local/bin
-mkdir -p $in
+mkdir -p "$in"
 export PATH="$PATH:$in"
 
 curl --silent --location \
     https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
-    TARGET=$in sh
+    TARGET="$in" sh
 
-ubi --project cli/cli --exe gh --in $in
-ubi --project houseabsolute/omegasort --in $in
-ubi --project houseabsolute/precious --in $in
-ubi --project JanDeDobbeleer/oh-my-posh --in $in
-ubi --project jqlang/jq --in $in
-ubi --project junegunn/fzf --in $in
-ubi --project mgdm/htmlq --in $in
-ubi --project oalders/is --in $in
-ubi --project sharkdp/bat --in $in
+projects=(
+    eza-community/eza
+    houseabsolute/omegasort
+    houseabsolute/precious
+    JanDeDobbeleer/oh-my-posh
+    jqlang/jq
+    junegunn/fzf
+    mgdm/htmlq
+    oalders/is
+    sharkdp/bat
+)
+
+for project in "${projects[@]}"; do
+    ubi --project "$project" --in "$in"
+done
+
+ubi --project cli/cli --exe gh --in "$in"
 
 ./installer/claude.sh
 ./installer/symlinks.sh
 ./configure/git.sh
-
-exit 0
